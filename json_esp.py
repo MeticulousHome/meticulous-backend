49c93d0939bde22bf60d738df648b37fc4fd2699
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2022-10-26 11:42:32 -0500

    This scrip sends and recives the json file correctly

--- json_esp.py
+++ json_esp.py
@@ -1,33 +1,26 @@
-import time
 import json
+import time
 import serial
-from pprint import pprint
-import random
+
 
 if __name__ == "__main__":
     print ("Ready...")
-    ser  = serial.Serial("COM3", baudrate= 115200, 
-           timeout=2.5, 
-           parity=serial.PARITY_NONE, 
-           bytesize=serial.EIGHTBITS, 
-           stopbits=serial.STOPBITS_ONE
-        )
+    ser  = serial.Serial("COM3", baudrate= 115200)
     f = open('fika.json','r')
     data = json.load(f)
-    # data["operation"] = "sequence"
-    # data["operation"] = "Italyan"
 
-    data=json.dumps(data)
-    # print (data)
-    if ser.isOpen():
-        ser.write(data.encode('ascii'))
-        ser.flush()
-        try:
-            incoming = ser.readline().decode("ascii")
-            print (incoming)
-        except Exception as e:
-            print (e)
-            pass
-        ser.close()
-    else:
-        print ("opening error")
+    data = json.dumps(data,indent = 1)
+    data = "*"+data+"#"
+    ser.write(data.encode("utf-8"))
+    init_while1 = True
+    while (init_while1):
+        if ser.inWaiting() > 0:
+            data_from_esp32 = ser.read().decode('utf-8')
+            print(data_from_esp32, end="")
+            if  data_from_esp32 == "#":
+                init_while1 = False
+
+    # while (True):
+    #     if ser.inWaiting() > 0:
+    #         data_from_esp32 = ser.read().decode('utf-8')
+    #         print(data_from_esp32, end="")
