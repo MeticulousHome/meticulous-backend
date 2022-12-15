82392833399b84c809335a6f7431a528ff775757
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2022-12-15 11:20:51 -0600

    fixed error file is not a json

--- back.py
+++ back.py
@@ -107,33 +107,33 @@ def msg(sid, data):
     # arduino.write(json_data.encode("utf-8"))
     if (data == "breville"):
         with open('./presets/breville.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     elif (data == "cube"):
         with open('./presets/cube.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     elif (data == "diletta"):
         with open('./presets/diletta.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     elif (data == "flair"):
         with open('./presets/flair.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     elif (data == "la-pavoni"):
         with open('./presets/la-pavoni.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     elif (data == "rocket"):
         with open('./presets/rocket.json','r',encoding="utf-8") as file:
-            json_data = json.dumps(data, indent=1, sort_keys=False)
-            json_data = "json\n"+file+"\x03"
+            json_data = json.dumps(file.read(), indent=1, sort_keys=False)
+            json_data = "json\n"+json_data+"\x03"
             arduino.write(json_data.encode("utf-8"))
     else:
         print("Preset not valid")
