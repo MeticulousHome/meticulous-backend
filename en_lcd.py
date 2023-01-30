4baac582a231c6974915178c11bb43e205a5db39
unknown <angel_osorno@outlook.com>
2023-01-30 10:56:39 -0600

    enable lcd with 0

--- en_lcd.py
+++ en_lcd.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
+from dotenv import load_dotenv
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
-GPIO.output(25, 1)
+if os.environ.get("PCB_VERSION") == "V3.4":
+    GPIO.output(lcd_en, 0)
+else:
+    GPIO.output(25, 1)
 
