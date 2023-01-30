b5ff2d74aa3ff2f77f75b03eaab88874fc8a8f2c
unknown <angel_osorno@outlook.com>
2023-01-30 10:49:39 -0600

    enable lcd with 0

--- lcd.py
+++ lcd.py
@@ -1,6 +1,9 @@
 import serial
 import time
 import RPi.GPIO as GPIO
+from dotenv import load_dotenv
+
+load_dotenv()
 
 lcd_en = 25
 GPIO.setmode(GPIO.BCM)
@@ -8,6 +11,9 @@ GPIO.setup(lcd_en, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(lcd_en, 1)
+        if os.environ.get("PCB_VERSION") == "V3.4":
+            GPIO.output(lcd_en, 0)
+        else:
+            GPIO.output(lcd_en, 1)
 except KeyboardInterrupt:
     GPIO.cleanup()
