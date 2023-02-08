16b8e155e506cb037e6920ce7fe8a7a3658777dc
unknown <angel_osorno@outlook.com>
2023-02-07 18:01:30 -0600

    Change enviroment variables names

--- en_lcd.py
+++ en_lcd.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
-if os.environ.get("PCB_VERSION") == "V3.4":
+if os.environ.get("SWITCH_VERSION") == "V3.4":
     GPIO.output(25, 0)
+    print("SWITCH_VERSION = V3.4")
 else:
     GPIO.output(25, 1)
 
