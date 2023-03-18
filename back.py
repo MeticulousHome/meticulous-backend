2034c3f3b59a15d1de1595c3d3fda5e0574778b2
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2023-03-18 14:34:35 -0600

    delay 0.5 only in start

--- back.py
+++ back.py
@@ -207,7 +207,11 @@ def disconnect(sid):
 
 @sio.on('action')
 def msg(sid, data):
-    time.sleep(0.5)
+    if data == "start":
+        start_function()
+        time.sleep(0.5)
+    
+    time.sleep(0.05)
     data = "action,"+data+"\x03"
     print(data)
     arduino.write(data.encode("utf-8"))
