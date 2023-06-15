23605f54d12caf5c26ec363d22f3ef24ec36ed27
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2023-06-15 13:41:54 -0600

    A function was added that returns the dictionary of a JSON

---
+++ stages/stages.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+import os
+import json
+
+def select_json_structure(option):
+    try:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(dir_path, option + ".json")
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"El archivo '{option}' no existe.")
+    except json.JSONDecodeError:
+        print(f"El archivo '{option}' no es un JSON válido.")
+
+if __name__ == "__main__":
+    print(select_json_structure("purge"))
