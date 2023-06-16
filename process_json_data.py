3b5e8a3fc06da978f2518c1bd85a7afe718a4c5a
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2023-06-16 11:55:05 -0600

    get curve modifies the values of preinfusion curve

--- process_json_data.py
+++ process_json_data.py
@@ -2,8 +2,17 @@ import json
 import stages.stages as stages
 
 def get_curve(kind,payload,init_node,end_node):
-    print(init_node)
-    print(end_node)
+    if kind == "Preinfusion":
+        json_preinfusion = stages.select_json_structure("curve")
+        #put a for here
+        for key in payload:
+            json_preinfusion[key] = payload[key]
+        print(json_preinfusion)
+    if kind == "Infusion":
+        json_preinfusion = stages.select_json_structure("curve")
+    if kind == "spring":
+        json_preinfusion = stages.select_json_structure("spring_1")
+
 
 def generate_json(payload):
     init_node = 300
@@ -12,12 +21,12 @@ def generate_json(payload):
     #Hacemos un for para recorrer los stages
     for stage in stages:
         if stage["name"] == "Preinfusion":
-            preinfusion = get_curve("Preinfusion",[stage],init_node,end_node)
+            preinfusion = get_curve("Preinfusion",stage,init_node,end_node)
             pass
         if stage["name"] == "Infusion":
-            Infusion = get_curve("Preinfusion",[stage],init_node+1,end_node+1)
+            Infusion = get_curve("Infusion",stage,init_node+1,end_node+1)
         if stage["name"] == "spring":
-            spring = get_curve("Preinfusion",[stage],init_node+1,end_node+1)
+            spring = get_curve("spring",stage,init_node+1,end_node+1)
         
 
 if __name__ == "__main__":
