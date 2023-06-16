dffc6f5481a4bfbc974b7a15abbaeb33f8adc056
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2023-06-16 11:07:10 -0600

    init node and end node were added

--- process_json_data.py
+++ process_json_data.py
@@ -1,20 +1,23 @@
 import json
 import stages.stages as stages
 
-def get_curve(payload):
-    print(payload)
+def get_curve(kind,payload,init_node,end_node):
+    print(init_node)
+    print(end_node)
 
 def generate_json(payload):
+    init_node = 300
+    end_node = init_node + 1
     stages = payload["stages"]
     #Hacemos un for para recorrer los stages
     for stage in stages:
         if stage["name"] == "Preinfusion":
-            preinfusion = get_curve([stage])
+            preinfusion = get_curve("Preinfusion",[stage],init_node,end_node)
             pass
         if stage["name"] == "Infusion":
-            Infusion = get_curve([stage])
+            Infusion = get_curve("Preinfusion",[stage],init_node+1,end_node+1)
         if stage["name"] == "spring":
-            spring = get_curve([stage])
+            spring = get_curve("Preinfusion",[stage],init_node+1,end_node+1)
         
 
 if __name__ == "__main__":
