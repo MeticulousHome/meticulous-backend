cae9bdf001bc5671d65f02dc6e8c55f2237e95b6
Oscar León Rodríguez <oscarleon.ro16@gmail.com>
2023-06-16 10:49:32 -0600

    ifs were added

--- process_json_data.py
+++ process_json_data.py
@@ -1,23 +1,25 @@
 import json
 import stages.stages as stages
 
-
+def get_curve(payload):
+    print(payload)
 
 def generate_json(payload):
-    if payload.get("source") == "DASHBOARD":
-        print("Coming from DASHBOARD")
-        # Search if the value "spring" exists anywhere in the dictionary
-        found = stages.find_key_value(payload, "spring")
-        if found:
-            print("The value 'spring' was found")
-        else:
-            print("The value 'spring' was not found")
+    stages = payload["stages"]
+    #Hacemos un for para recorrer los stages
+    for stage in stages:
+        if stage["name"] == "Preinfusion":
+            preinfusion = get_curve([stage])
+            pass
+        if stage["name"] == "Infusion":
+            Infusion = get_curve([stage])
+        if stage["name"] == "spring":
+            spring = get_curve([stage])
         
-    # You can add more logic here if you need to process other sources besides "DASHBOARD"
 
 if __name__ == "__main__":
     # Read the JSON file
-    with open("./json/dashboard_1_0.json", "r") as f:
+    with open("dashboard.json", "r") as f:
         data = json.load(f)
 
     # Call the function with the loaded data
