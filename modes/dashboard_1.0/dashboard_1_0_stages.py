5d8eb942aa5153c63c954f4d21e1606eccabf2dc
AUOR17 <angel_osorno@outlook.com>
2023-09-11 22:33:37 -0600

    Added curves stages in dashboard

--- modes/dashboard_1.0/dashboard_1_0_stages.py
+++ modes/dashboard_1.0/dashboard_1_0_stages.py
@@ -5,6 +5,7 @@ from retracting_stage import get_retracting_stage as get_retracting_stage
 from closing_valve_stage import get_closing_valve_stage as get_closing_valve_stage
 from idle_stage import get_idle_stage as get_idle_stage
 from retracting_2_stage import get_retracting_2_stage as get_retracting_2_stage
+from curve_stages import get_curve_stages as get_curve_stages
 
 def get_stages(parameters: json):    
     stages = []
@@ -19,6 +20,8 @@ def get_stages(parameters: json):
     current_stage += 1
     closing_valve_stage = get_closing_valve_stage(parameters, current_stage, current_stage + 1)
     current_stage += 1
+    curve_stages = get_curve_stage(parameters, current_stage, current_stage + 1)
+    current_stage += 1
     idle_stage = get_idle_stage(parameters, current_stage, current_stage + 1)
     current_stage += 1
     retracting_2_stage = get_retracting_2_stage(parameters, current_stage, final_node)
@@ -28,6 +31,7 @@ def get_stages(parameters: json):
     stages.extend(heating_stage if isinstance(heating_stage, list) else [heating_stage])
     stages.append(retracting_stage)
     stages.append(closing_valve_stage)
+    stages.append(curve_stages)
     stages.append(idle_stage)
     stages.append(retracting_2_stage)
 
