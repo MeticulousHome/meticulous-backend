3cf7be8928ce9bf4d3ae4c1bb9da91f1447705e3
Angel Padilla <angel.p.lopez@outlook.com>
2023-09-13 15:39:02 -0600

    refactor(back.py): refactored GPIO init
    GPIO pins are initialized within a function. All the pins initialized are
    hold in a dictionary in order to pass them if needed to any function/module

--- back.py
+++ back.py
@@ -19,33 +19,7 @@ import hashlib
 import version as backend
 import subprocess
 
-comando = './clean_logs.sh' #Changue to use reduced path.
-lock = threading.Lock()    
-file_path = './logs/'       #Change to use reduced path.
-buffer=""
-contador= 'contador.txt'
-autoupdate_path = "~/meticulous-raspberry-setup/meticulous-autoupdate"
-
-usaFormatoDeColores = True
-
-sendInfoToFront = False
-
-infoReady = False
-
-lastJSON_source = "LCD"
-
-#VERSION INFORMATION
-
-borrarFormato = "\033[0m"
-colores = [
-    "\033[1;31m", # Rojo
-    "\033[1;32m", # Verde
-    "\033[1;33m", # Amarillo
-    "\033[1;34m", # Azul
-    "\033[1;35m", # Morado
-    "\033[1;36m", # Cian
-]
-
+#CLASSES DEFINITIONS
 class ReadLine:
     def __init__(self, s):
         self.buf = bytearray()
@@ -68,134 +42,9 @@ class ReadLine:
             else:
                 self.buf.extend(data)
 
-#load_dotenv()####################!!!!No libreria en som#
+#GLOBAL VARIABLES DECLARATIONS
 
-
-# lcd_en = 25
-# esp_en = 8
-    
-# if os.environ.get("PINES_VERSION") == "V3":
-#     en = 27
-#     io0 = 17
-#     print("Set pines to V3") 
-# elif os.environ.get("PINES_VERSION") == "V3.1":
-#     en = 24
-#     io0 = 23
-#     print("Set pines to V3.1") 
-# else:
-#     en = 24
-#     io0 = 23
-#     print("Set pines to V3.1") 
-    
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(en, GPIO.OUT)
-# GPIO.setup(io0, GPIO.OUT)
-# GPIO.setup(esp_en, GPIO.OUT)
-# GPIO.setup(lcd_en, GPIO.OUT)  ###################### Debera haber un if que confirme en el entorno (raspberry o VAR-SOM-MX8M-NANO)
-chip0 = gpiod.chip('gpiochip0') ####################### En base a ello definir el comando adecuado para controlar gpio's
-chip4 = gpiod.chip('gpiochip4')
-chip3 = gpiod.chip('gpiochip3')
-
-def createUpdateDir():
-    # Specify the directory path you want to create
-    directory_path = os.path.expanduser("~/update")
-
-    # Check if the directory already exists
-    if not os.path.exists(directory_path):
-    # Create the directory if it does not exist
-        os.makedirs(directory_path)
-        #print(f"Directory '{directory_path}' created successfully.")
-config = gpiod.line_request()
-config.consumer = 'myapp'
-config.request_type = gpiod.line_request.DIRECTION_OUTPUT
-
-# Initialize GPIO lines
-#lcd_en = chip0.get_line(13)  
-esp_en = chip4.get_line(9)
-en = chip0.get_line(7)  
-io0 = chip0.get_line(8)
-buffer_pin = chip3.get_line(26)
-
-lines = [esp_en, en, io0, buffer_pin]
-for line in lines:
-    try:
-        line.request(config)
-    except OSError:
-        print(f"Error: pin {line.offset()} could not be set to output")
-
-def gatherVersionInfo():
-    global infoSolicited
-    software_info["name"] = "Meticulous Espresso"
-    software_info["backendV"] = backend.VERSION
-
-    # #OBTENEMOS EL NOMBRE DE LA APLICACION DE LA LCD
-    # auxFile = open(os.path.expanduser("~/.xsession"))
-    # lcd_ui_name = auxFile.read().split('\n')[2].split()[1]
-
-    # #OBTENEMOS SU VERSION USANDO LOS COMANDOS DPKG y GREP
-    # command = f'dpkg --list | grep {lcd_ui_name}'
-    # result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    # lcd_version = result.stdout.split()[2]
-    lcd_version = 1.0 #HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    ##############################################Provisionalmente y al no haber una version de la LCD, se asigna la version 1.0
-    infoSolicited = True
-
-    software_info["lcdV"] = lcd_version
-
-    software_info["dashboardV"] = 1.0
-    software_info["firmwareV"] = 0.0
-
-    #SOLICITAMOS LA VERSION DE FIRMWARE A LA ESP
-
-def turn_on():
-    # if os.environ.get("EN_PIN_HIGH") == "0":
-    #     GPIO.output(esp_en, 0)
-    #     GPIO.output(lcd_en, 0)
-    #     print("EN_PIN_HIGH = 0")
-    # elif os.environ.get("EN_PIN_HIGH") == "1":
-    #     GPIO.output(esp_en, 1)
-    #     GPIO.output(lcd_en, 1)
-    #     print("EN_PIN_HIGH = 1")
-    # else:
-    #     GPIO.output(esp_en, 0)
-    #     GPIO.output(lcd_en, 0)
-    #     print("EN_PIN_HIGH = 0 por default")########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
-    esp_en.set_value(0) ##############################
-    buffer_pin.set_value(0)
-    # lcd_en.set_value(0)
-    print("EN_PIN_HIGH = 0 por default")
-
-
-def turn_off():
-    # if os.environ.get("EN_PIN_HIGH") == "0":
-    #     GPIO.output(esp_en, 1)
-    #     GPIO.output(lcd_en, 1)
-    #     print("EN_PIN_HIGH = 0")
-    # elif os.environ.get("EN_PIN_HIGH") == "1":
-    #     GPIO.output(esp_en, 0)
-    #     GPIO.output(lcd_en, 0)
-    #     print("EN_PIN_HIGH = 1")
-    # else:
-    #     GPIO.output(esp_en, 1)
-    #     GPIO.output(lcd_en, 1)
-    #     print("EN_PIN_HIGH = 0 por default")########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
-    esp_en.set_value(1)##############################
-    buffer_pin.set_value(1)
-    # lcd_en.set_value(1)
-    print("EN_PIN_HIGH = 0 por default")    
-
-turn_on()
-#os.system('killall coffee-ui-demo')
-#time.sleep(5)
-
-keyboard = Controller()
-
-
-define("port", default=8080, help="run on the given port", type=int)
-define("debug", default=False, help="run in debug mode")
-
-sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='tornado')
-
+#data variables
 data_sensors = {
     "pressure":1,
     "flow":2,
@@ -241,6 +90,172 @@ software_info = {
     "backendV": 5,
     "fanStatus": "on",
 }
+
+#general purpose variables
+comando = './clean_logs.sh' #Changue to use reduced path.
+lock = threading.Lock()    
+file_path = './logs/'       #Change to use reduced path.
+buffer=""
+contador= 'contador.txt'
+
+usaFormatoDeColores = True
+
+sendInfoToFront = False
+
+infoReady = False
+
+lastJSON_source = "LCD"
+
+sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='tornado')
+
+keyboard = Controller()
+
+#pin variables declaration
+pins = {}########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
+
+#chip variables declaration
+chip0 = None ########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
+chip3 = None
+chip4 = None
+
+# formatting
+borrarFormato = "\033[0m"
+colores = [
+    "\033[1;31m", # Rojo
+    "\033[1;32m", # Verde
+    "\033[1;33m", # Amarillo
+    "\033[1;34m", # Azul
+    "\033[1;35m", # Morado
+    "\033[1;36m", # Cian
+]
+
+#FUNCTION DEFINITIONS
+#load_dotenv()####################!!!!No libreria en som#
+def createUpdateDir():
+    # Specify the directory path you want to create
+    directory_path = os.path.expanduser("~/update")
+
+    # Check if the directory already exists
+    if not os.path.exists(directory_path):
+    # Create the directory if it does not exist
+        os.makedirs(directory_path)
+        #print(f"Directory '{directory_path}' created successfully.")
+
+def initialize_GPIO():
+    global pins
+    global chip0
+    global chip3
+    global chip4
+    # lcd_en = 25
+    # esp_en = 8
+        
+    # if os.environ.get("PINES_VERSION") == "V3":
+    #     en = 27
+    #     io0 = 17
+    #     print("Set pines to V3") 
+    # elif os.environ.get("PINES_VERSION") == "V3.1":
+    #     en = 24
+    #     io0 = 23
+    #     print("Set pines to V3.1") 
+    # else:
+    #     en = 24
+    #     io0 = 23
+    #     print("Set pines to V3.1") 
+        
+    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setup(en, GPIO.OUT)
+    # GPIO.setup(io0, GPIO.OUT)
+    # GPIO.setup(esp_en, GPIO.OUT)
+    # GPIO.setup(lcd_en, GPIO.OUT)  ###################### Debera haber un if que confirme en el entorno (raspberry o VAR-SOM-MX8M-NANO)
+    chip0 = gpiod.chip('gpiochip0') ####################### En base a ello definir el comando adecuado para controlar gpio's
+    chip4 = gpiod.chip('gpiochip4')
+    chip3 = gpiod.chip('gpiochip3')
+
+    config = gpiod.line_request()
+    config.consumer = 'backend'
+    config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+
+    # Initialize GPIO lines
+    #lcd_en = chip0.get_line(13)  
+    pins["esp_en"]= chip4.get_line(9)
+    pins["en"]= chip0.get_line(7)  
+    pins["io0"]= chip0.get_line(8)
+    pins["buffer_pin"]= chip3.get_line(26)
+
+    for key,pin in pins:
+        try:
+            pin.request(config)
+        except OSError:
+            print(f"Error:'{key}' pin at {pin.offset()} could not be set to output")
+
+def close_GPIO_chips():
+    global chip0
+    global chip3
+    global chip4
+
+    chip0.close()
+    chip3.close()
+    chip4.close()
+
+def gatherVersionInfo():
+    global infoSolicited
+    software_info["name"] = "Meticulous Espresso"
+    software_info["backendV"] = backend.VERSION
+
+    # #OBTENEMOS EL NOMBRE DE LA APLICACION DE LA LCD
+    # auxFile = open(os.path.expanduser("~/.xsession"))
+    # lcd_ui_name = auxFile.read().split('\n')[2].split()[1]
+
+    # #OBTENEMOS SU VERSION USANDO LOS COMANDOS DPKG y GREP
+    # command = f'dpkg --list | grep {lcd_ui_name}'
+    # result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    # lcd_version = result.stdout.split()[2]
+    lcd_version = 1.0 #HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ##############################################Provisionalmente y al no haber una version de la LCD, se asigna la version 1.0
+    infoSolicited = True
+
+    software_info["lcdV"] = lcd_version
+
+    software_info["dashboardV"] = 1.0
+    software_info["firmwareV"] = 0.0
+
+    #SOLICITAMOS LA VERSION DE FIRMWARE A LA ESP
+
+def turn_on():
+    # if os.environ.get("EN_PIN_HIGH") == "0":
+    #     GPIO.output(esp_en, 0)
+    #     GPIO.output(lcd_en, 0)
+    #     print("EN_PIN_HIGH = 0")
+    # elif os.environ.get("EN_PIN_HIGH") == "1":
+    #     GPIO.output(esp_en, 1)
+    #     GPIO.output(lcd_en, 1)
+    #     print("EN_PIN_HIGH = 1")
+    # else:
+    #     GPIO.output(esp_en, 0)
+    #     GPIO.output(lcd_en, 0)
+    #     print("EN_PIN_HIGH = 0 por default")########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
+    pins["esp_en"].set_value(0) ##############################
+    pins["buffer_pin"].set_value(0)
+    # lcd_en.set_value(0)
+    print("EN_PIN_HIGH = 0 por default")
+
+def turn_off():
+    # if os.environ.get("EN_PIN_HIGH") == "0":
+    #     GPIO.output(esp_en, 1)
+    #     GPIO.output(lcd_en, 1)
+    #     print("EN_PIN_HIGH = 0")
+    # elif os.environ.get("EN_PIN_HIGH") == "1":
+    #     GPIO.output(esp_en, 0)
+    #     GPIO.output(lcd_en, 0)
+    #     print("EN_PIN_HIGH = 1")
+    # else:
+    #     GPIO.output(esp_en, 1)
+    #     GPIO.output(lcd_en, 1)
+    #     print("EN_PIN_HIGH = 0 por default")########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
+    pins["esp_en"].set_value(1)##############################
+    pins["buffer_pin"].set_value(1)
+    # lcd_en.set_value(1)
+    print("EN_PIN_HIGH = 0 por default")    
 
 # "d" -> double click tare
 # "s" -> long tare
@@ -305,11 +320,11 @@ def reboot_esp():
     # GPIO.output(en, 0)
     # time.sleep(.1)
     # GPIO.output(en, 1)#########Se debera determinar el entorno (raspberry o VAR-SOM-MX8M-NANO)
-    en.set_value(1)##############
-    io0.set_value(1)
+    pins["en"].set_value(1)##############
+    pins["io0"].set_value(1)
     time.sleep(.1)
-    en.set_value(0)
-    io0.set_value(0)
+    pins["en"].set_value(0)
+    pins["io0"].set_value(0)
 
 def poweroff_esp():
     GPIO.output(en, 0)
@@ -365,131 +380,6 @@ def detect_source(json_data):
     #print(f'{source},{preinfusion_10},{preinfusion_11},{infusion_13},{infusion_20}')
     #print(source)
     return source
-
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
-#sendInfoToFront
-@sio.on('action')
-def msg(sid, data):
-    if data == "start":
-        time.sleep(0.5)
-        data = "action,"+data+"\x03"
-        print(data)
-        arduino.write(data.encode("utf-8"))
-    else:
-        time.sleep(0.05)
-        data = "action,"+data+"\x03"
-        print(data)
-        arduino.write(data.encode("utf-8"))
-
-@sio.on('endTransmition')
-def endRecept(sid):
-    print("File received")
-    tr = threading.Thread(target=startUpdate)
-    tr.start()
-
-@sio.on('UpdateFile')
-def rcvPckg(sid, data):
-    createUpdateDir()
-    #receive and create a file to save the udpateFile
-    with open(os.path.expanduser("~/update/updtPckg.tar.gz"), 'ab') as file:
-        file.write(data)
-    #Create a thread that will decompress the data and call the updater script
-
-@sio.on('askForInfo')
-def setSendInfo(sid):
-    global sendInfoToFront
-    sendInfoToFront = True
-
-@sio.on('stopInfo')
-def StopInfo(sid):
-    global sendInfoToFront
-    sendInfoToFront = False
-
-@sio.on('toggle-fans')
-def toggleFans(sid, data):
-    _solicitud = ""
-    if data:
-        print("fans on")
-        _solicitud ="action,fans-on\x03"
-    else:
-        print("fans off")
-        _solicitud = "action,fans-off\x03"
-    arduino.write(str.encode(_solicitud))
-    software_info["fanStatus"] = 'on' if data else 'off'
-
-@sio.on('parameters')
-def msg(sid, data):
-    global lastJSON_source
-    json_data = json.dumps(data, indent=1, sort_keys=False)
-    send_json_hash(json_data)
-    lastJSON_source = detect_source(data)
-    print(lastJSON_source)
-
-@sio.on('send_profile')
-async def forwardJSON(sid,data):
-    print(json.dumps(data, indent=1, sort_keys=False))
-    await sio.emit('save_profile', data)
-
-@sio.on('preset')
-def msg(sid, data):
-    global lastJSON_source
-    # data = data + "hola mundo"
-    if (data == "breville"):
-        preset="breville.json"
-    
-    elif (data == "cube"):
-        preset="cube.json"
-
-    elif (data == "diletta"):
-        preset="diletta.json"
-
-    elif (data == "flair"):
-        preset="flair.json"
-
-    elif (data == "la-pavoni"):
-        preset="la-pavoni.json"
-
-    elif (data == "rocket"):
-        preset="rocket.json"
-
-    else:
-        print("Preset not valid")
-        return 0
-
-    try:
-        with open('./presets/'+ preset ,'r',encoding="utf-8") as file:
-            json_data = json.load(file)
-            json_data = json.dumps(json_data, indent=1,sort_keys=False)
-            send_json_hash(json_data)
-            lastJSON_source = detect_source(json_data)
-            #send the instruccion to start the selected choice
-            _input = "action,"+"start"+"\x03"
-            arduino.write(str.encode(_input))
-    except:
-        print("Preset not found")
-        return 0
-
-
-
-#@sio.on('parameters') #To hardcode using send config
-
-#data seems to not be used but is kept as optional to ease implementation if needed
-@sio.on('calibrate') #Use when calibration it is implemented
-def msg(sid, data=True):
-    know_weight = "100.0"
-    current_weight = data_sensors["weight"]
-    data ="calibration"+","+know_weight+","+str(current_weight)
-    _input = "action,"+data+"\x03"
-    arduino.write(str.encode(_input))
-
-
-
 
 # arduino = serial.Serial("COM4",115200)
 # arduino = serial.Serial('/dev/ttyS0',115200)
@@ -907,13 +797,142 @@ def startUpdate():
     
     #we dont stop the lcd yet to use it as the only communication channel available
     
-    #call the update script
+    #call the update script (will use the script as a module)
     command = f'cd {autoupdate_path} && python update_protocol.py'
     update_success = subprocess.run(command, shell=True, capture_output=True, text=True).stdout
 
     print(update_success)
 
 
+#SOCKET CALLBACKS
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+#sendInfoToFront
+@sio.on('action')
+def msg(sid, data):
+    if data == "start":
+        time.sleep(0.5)
+        data = "action,"+data+"\x03"
+        print(data)
+        arduino.write(data.encode("utf-8"))
+    else:
+        time.sleep(0.05)
+        data = "action,"+data+"\x03"
+        print(data)
+        arduino.write(data.encode("utf-8"))
+        
+@sio.on('endTransmition')
+def endRecept(sid):
+    print("File received")
+    tr = threading.Thread(target=startUpdate)
+    tr.start()
+
+@sio.on('UpdateFile')
+def rcvPckg(sid, data):
+    createUpdateDir()
+    #receive and create a file to save the udpateFile
+    with open(os.path.expanduser("~/update/updtPckg.tar.gz"), 'ab') as file:
+        file.write(data)
+    #Create a thread that will decompress the data and call the updater script
+
+@sio.on('askForInfo')
+def setSendInfo(sid):
+    global sendInfoToFront
+    sendInfoToFront = True
+
+@sio.on('stopInfo')
+def StopInfo(sid):
+    global sendInfoToFront
+    sendInfoToFront = False
+
+@sio.on('toggle-fans')
+def toggleFans(sid, data):
+    _solicitud = ""
+    if data:
+        print("fans on")
+        _solicitud ="action,fans-on\x03"
+    else:
+        print("fans off")
+        _solicitud = "action,fans-off\x03"
+    arduino.write(str.encode(_solicitud))
+    software_info["fanStatus"] = 'on' if data else 'off'
+
+@sio.on('parameters')
+def msg(sid, data):
+    global lastJSON_source
+    json_data = json.dumps(data, indent=1, sort_keys=False)
+    send_json_hash(json_data)
+    lastJSON_source = detect_source(data)
+    print(lastJSON_source)
+
+@sio.on('send_profile')
+async def forwardJSON(sid,data):
+    print(json.dumps(data, indent=1, sort_keys=False))
+    await sio.emit('save_profile', data)
+
+@sio.on('preset')
+def msg(sid, data):
+    global lastJSON_source
+    # data = data + "hola mundo"
+    if (data == "breville"):
+        preset="breville.json"
+    
+    elif (data == "cube"):
+        preset="cube.json"
+
+    elif (data == "diletta"):
+        preset="diletta.json"
+
+    elif (data == "flair"):
+        preset="flair.json"
+
+    elif (data == "la-pavoni"):
+        preset="la-pavoni.json"
+
+    elif (data == "rocket"):
+        preset="rocket.json"
+
+    else:
+        print("Preset not valid")
+        return 0
+
+    try:
+        with open('./presets/'+ preset ,'r',encoding="utf-8") as file:
+            json_data = json.load(file)
+            json_data = json.dumps(json_data, indent=1,sort_keys=False)
+            send_json_hash(json_data)
+            lastJSON_source = detect_source(json_data)
+            #send the instruccion to start the selected choice
+            _input = "action,"+"start"+"\x03"
+            arduino.write(str.encode(_input))
+    except:
+        print("Preset not found")
+        return 0
+
+#@sio.on('parameters') #To hardcode using send config
+
+#data seems to not be used but is kept as optional to ease implementation if needed
+@sio.on('calibrate') #Use when calibration it is implemented
+def msg(sid, data=True):
+    know_weight = "100.0"
+    current_weight = data_sensors["weight"]
+    data ="calibration"+","+know_weight+","+str(current_weight)
+    _input = "action,"+data+"\x03"
+    arduino.write(str.encode(_input))
+
+initialize_GPIO()
+turn_on()
+#os.system('killall coffee-ui-demo')
+#time.sleep(5)
+
+
+define("port", default=8080, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
 
 
 if __name__ == "__main__":
