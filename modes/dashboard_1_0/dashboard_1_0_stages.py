05fb156259002ef1533b56b2fc35c24cc6483bd5
oscarlr16 <oscarleon.ro16@gmail.com>
2023-09-19 11:06:39 -0600

    fix(dashboard_1_0_stages.py): use get_curve_stage function

--- modes/dashboard_1_0/dashboard_1_0_stages.py
+++ modes/dashboard_1_0/dashboard_1_0_stages.py
@@ -5,7 +5,7 @@ from .retracting_stage import get_retracting_stage as get_retracting_stage
 from .closing_valve_stage import get_closing_valve_stage as get_closing_valve_stage
 from .idle_stage import get_idle_stage as get_idle_stage
 from .retracting_2_stage import get_retracting_2_stage as get_retracting_2_stage
-from .curve_stages import get_curve_stages as get_curve_stages
+from .curve_stages import get_curve_stage as get_curve_stage
 
 def get_stages(parameters: json):    
     stages = []
@@ -31,7 +31,7 @@ def get_stages(parameters: json):
     stages.extend(heating_stage if isinstance(heating_stage, list) else [heating_stage])
     stages.append(retracting_stage)
     stages.append(closing_valve_stage)
-    stages.append(curve_stages)
+    stages.extend(curve_stages if isinstance(curve_stages, list) else [curve_stages])
     stages.append(idle_stage)
     stages.append(retracting_2_stage)
 
