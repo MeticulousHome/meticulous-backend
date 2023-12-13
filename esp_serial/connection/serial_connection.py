01a1193d332bcb3cf75acd4f2b9cf3333197d589
Mimoja <mimoja@meticuloushome.com>
2023-12-12 22:39:40 -0600

    esp: Move connection handeling into its own class
    So far the ESP32's connection-handling lived inside the back.py main file.
    This comes with a multitude of downsides, including the inability to unit
    test certain components but also the dependencies on the specific GPIO
    pins that the current fika board uses. It is therefore impossible to connect
    a USB<->UART connector to the fika board to just talk to the ESP32 from another
    machine for development efforts.
    Move the connection logic out of the main file and into its own class for
    reusability and encapsulation.
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ esp_serial/connection/serial_connection.py
@@ -0,0 +1,62 @@
+import serial, serial.tools.list_ports
+
+from log import MeticulousLogger
+logger = MeticulousLogger.getLogger(__name__)
+
+try:
+    if "serialization" in serial.__doc__ and "deserialization" in serial.__doc__:
+        raise ImportError(
+            "The backend depends on pyserial which conflicts serial"
+        )
+except TypeError:
+    pass  # __doc__ returns None for pyserial
+
+class SerialConnection():
+    BAUDRATE = 115200
+    CONNECT_TIMEOUT = 2
+
+    def __init__(self, device) -> None:
+        self.device = None
+        self.port = None
+        self.connected = False
+
+        if self.connect(device) is None or not self.connected:
+            raise RuntimeError("Couldn't connect to ESP32");
+
+        logger.info("An ESP32 is connected, resetting");
+
+        self.reset(bootloader=False)
+
+    def available(self):
+        return self.port != None and self.connected
+
+    def connect(self, devices):
+        if not isinstance(devices, list):
+            devices = [devices]
+
+        logger.debug(f"All serial devices to test: {devices}")
+        for port in devices:
+            logger.info(f"Testing connect to {port}");
+            try:
+                ser = serial.Serial(port, baudrate=SerialConnection.BAUDRATE, timeout=SerialConnection.CONNECT_TIMEOUT)
+                # FIXME In back.py:
+                # Read one byte or wait until timeout above is reached
+                # This test assues an existing firmware
+                self.device = port
+                self.port = ser
+                self.connected = True
+                logger.info(f"Successfully connected to {port}");
+                return port
+
+            except (OSError, serial.SerialException) as e:
+                logger.info(f"Serial Exception raised while trying to connect: {e}")
+        # If no Device was detected, return None
+        logger.warning("No serial Device found!")
+        return None
+
+    def disconnect(self):
+        if self.port:
+            self.port.close()
+
+    def reset(**args):
+        raise NotImplementedError()
