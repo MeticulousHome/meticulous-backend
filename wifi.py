ef05d97f164a68bb7fa3d6299fecfcc5acb2538e
Mimoja <mimoja@meticuloushome.com>
2023-12-13 17:03:06 -0600

    wifi: Add WifiManager class to connect to a wifi and get system status
    To be able to use the machine in production we need to be able to
    provision wifi. Instead of using the NetworkManager dbus API we are
    using a wrapper around the nmcli tool.
    This allows us to keep stability across different python versions
    as the respective python dbus projects are currently not very
    stable.
    
    So far the WifiManager supports:
    - Connecting to WPA-2/3 with PSK
    - Scan for available networks
    - Parsing the current network configuration into a dataclass
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ wifi.py
@@ -0,0 +1,102 @@
+from netaddr import IPAddress, IPNetwork
+from typing import List
+from dataclasses import dataclass
+
+import nmcli
+import time
+
+from log import MeticulousLogger
+logger = MeticulousLogger.getLogger(__name__)
+
+nmcli.disable_use_sudo()
+
+@dataclass
+class WifiSystemConfig():
+    """Class Representing the current network configuration"""
+    connected: bool
+    connection_name: str
+    gateway: IPAddress
+    routes: List[str]
+    ips: List[IPNetwork]
+    dns: List[IPAddress]
+    mac: str
+
+class WifiManager():
+    _known_wifis = []
+
+    def scanForNetworks(timeout: int = 10, target_network_ssid: str = None):
+        if target_network_ssid == "":
+            target_network_ssid = None
+
+        target_timeout = time.time() + timeout
+        while time.time() < target_timeout:
+            logger.info(
+                f"Requesting scan results: Time left: {target_timeout - time.time()}s")
+            wifis = []
+            try:
+                wifis = nmcli.device.wifi()
+            except Exception as e:
+                logger.info(
+                    f"Failed to scan for wifis: {e}, retrying if timeout is not reached")
+                wifis = []
+
+            if target_network_ssid != None:
+                wifis = [w for w in wifis if w.ssid == target_network_ssid]
+
+            if len(wifis) > 0:
+                break
+
+        WifiManager._known_wifis = wifis
+        return wifis
+
+    def connectToWifi(ssid: str, password: str):
+        logger.info(f"Connecting to wifi: {ssid}")
+
+        networks = WifiManager.scanForNetworks(
+            timeout=30, target_network_ssid=ssid)
+        logger.info(networks)
+        if len(networks) > 0:
+            logger.info("Target network online, connecting now")
+            try:
+                nmcli.device.wifi_connect(ssid, password)
+            except Exception as e:
+                logger.info(f"Failed to connect to wifi: {e}")
+                return False
+
+            if len([x for x in networks if x.in_use]) > 0:
+                logger.info("Successfully connected")
+                return True
+        return False
+
+    def getCurrentConfig() -> WifiSystemConfig:
+
+        connected: bool = False
+        connection_name: str = None
+        gateway: IPAddress = None
+        routes: list[str] = []
+        ips: list[IPNetwork] = []
+        dns: list[IPAddress] = []
+        mac: str = ""
+
+        for dev in nmcli.device():
+            if dev.device_type == 'wifi' and dev.state == "connected":
+                connected = True
+                config = nmcli.device.show(dev.device)
+                for k, v in config.items():
+                    match k:
+                        case str(k) if "IP4.ADDRESS" in k or "IP6.ADDRESS" in k:
+                            ip = IPNetwork(v)
+                            ips.append(ip)
+                        case str(k) if "IP4.ROUTE" in k or "IP6.ROUTE" in k:
+                            routes.append(v)
+                        case str(k) if "IP4.DNS" in k or "IP6.DNS" in k:
+                            ip = IPAddress(v)
+                            dns.append(ip)
+                        case "GENERAL.HWADDR":
+                            mac = v
+                        case "GENERAL.CONNECTION":
+                            connection_name = v
+                        case "IP4.GATEWAY":
+                            gateway = IPAddress(v)
+
+        return WifiSystemConfig(connected, connection_name, gateway, routes, ips, dns, mac)
