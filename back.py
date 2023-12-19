65c8a2918ada7520f2c450e46b44bae5ed171d05
Mimoja <mimoja@meticuloushome.com>
2023-12-19 17:25:42 +0100

    sensors: Introduce data classes for sensor data parsing
    Up until this point the parsing of the serial protocol happened
    in the main file back.py, leading to a crowded and difficult to read
    file.
    As sensor values are owned by the esps firmware there is no reason for
    them to be writable in the backend.
    We therefore move them into read-only dataclasses.
    The sensor data itself is never handled in the backend but instead only
    passed to the respective frontends via socket.io.
    The mapping for this was created when sending also in back.py.
    We move it to the respective data_classes for now so that we can later
    manipulate the mappings at one central point if we so desire.
    
    Dataclass fields do allow for additional metadata which in the future we can
    and should use for the sio mapping
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- back.py
+++ back.py
@@ -23,6 +23,8 @@ import base64
 
 from esp_serial.connection.usb_serial_connection import USBSerialConnection
 from esp_serial.connection.fika_serial_connection import FikaSerialConnection
+from esp_serial.data import *
+
 from ble_gatt import GATTServer
 
 from log import MeticulousLogger
@@ -114,54 +116,15 @@ define("debug", default=False, help="run in debug mode")
 
 sio = socketio.AsyncServer(cors_allowed_origins='*', async_mode='tornado')
 
-data_sensors = {
-    "pressure":1,
-    "flow":2,
-    "weight":3,
-    "temperature":4,
-    "status": "idle",
-    "time": 0,
-    "profile": "idle"
-}
-
-data_sensor_temperatures = {
-    "external_1":1,
-    "external_2":2,
-    "bar_up":3,
-    "bar_mid_up":4,
-    "bar_mid_down": 5,
-    "bar_down": 6,
-    "tube": 7,
-    "valve": 8
-}
-
-data_sensor_comunication = {
-    "preassure_sensor": 1,
-    "adc_0": 2,
-    "adc_1": 3,
-    "adc_2": 4,
-    "adc_3": 5
-}
-
-data_sensor_actuators = {
-    "motor_position": 1,
-    "motor_speed": 2,
-    "motor_power": 3,
-    "motor_current": 4,
-    "bandheater_power": 5
-}
+data_sensors = ShotData()
+sensor_sensors = None
+esp_info = None
 
 software_info = {
     "name": "Meticulous Espresso",
     "lcdV": 3,
-    "firmwareV": 4,
-    "backendV": 5,
-    "fanStatus": "on",
 }
 
-hardware_info = {
-    "mainVoltage": "240"
-}
 
 # "d" -> double click tare
 # "s" -> long tare
@@ -197,7 +160,7 @@ def encoder_push_function():
 def encoder_double_function():
     keyboard.press('x')
     keyboard.release('x')
-    if (data_sensors["status"] != "idle"):
+    if (data_sensors.status != "idle"):
         _input = "action,"+"stop"+"\x03"
         if(connection.port != None): connection.port.write(str.encode(_input))
     logger.info("DOUBLE ENCODER!")
@@ -339,7 +302,7 @@ def msg(sid, data):
 @sio.on('calibrate') #Use when calibration it is implemented
 def msg(sid, data=True):
     know_weight = "100.0"
-    current_weight = data_sensors["weight"]
+    current_weight = data_sensors.weight
     data ="calibration"+","+know_weight+","+str(current_weight)
     _input = "action,"+data+"\x03"
     if(connection.port != None): connection.port.write(str.encode(_input))
@@ -390,19 +353,22 @@ def read_arduino():
     global infoReady
     global stopESPcomm
     global sensor_status
+    global data_sensors
+    global sensor_sensors
+    global esp_info
 
     #Variables to save data
     idle_in_data = False
     save_str = False
     
-    start_time = time.time()
+    shot_start_time = time.time()
     # global start_time
 
     connection.port.reset_input_buffer()
     connection.port.write(b'32\n')
     uart = ReadLine(connection.port)
 
-    old_status = ""
+    old_status = MachineStatusEnum.IDLE
     time_flag = False
     while not stopESPcomm:
         data = uart.readline()
@@ -432,119 +398,80 @@ def read_arduino():
             if save_str or sensor_status:
                 logger.info(data_str.strip("\r\n"))
 
-            data_str_sensors = data_str.split(',')
+            data_str_sensors = data_str.strip("\r\n").split(',')
 
-            if data_str_sensors[0] == 'Data':
-                data_sensors["pressure"] = data_str_sensors[1]
-                data_sensors["flow"]= data_str_sensors[2]
-                data_sensors["weight"] = data_str_sensors[3]
-                data_sensors["temperature"] = data_str_sensors[4]
-                status_bad = data_str_sensors[5]
-                data_sensors["status"] = status_bad.strip("\n")
-                data_sensors["status"] = data_sensors["status"].strip("\r")
+            # potential message types
+            button_event = None
+            sensor = None
+            data = None
+            info = None
 
-                try:
-                    data_sensors["profile"] = data_str_sensors[6].strip("\n")
-                    data_sensors["profile"] = data_sensors["profile"].strip("\r")
-                except:
-                    data_sensors["profile"] = "None"
+            match(data_str_sensors):
+                # FIXME: This should be replace in the firmware with an "Event," prefix for cleanliness
+                case ["CCW" | "CW" | "push" | "pu_d" | "elng" | "ta_d" | "ta_l" | "strt"] as ev:
+                    button_event = ButtonEventData.from_args(ev)
+                case ["Event", *eventData]:
+                    button_event = ButtonEventData.from_args(eventData)
+                case ["Data", *dataArgs]:
+                    data = ShotData.from_args(dataArgs)
+                case ["Sensors", colorCodedString]:
+                    sensor = SensorData.from_color_coded_args(colorCodedString)
+                case ["Sensors", *sensorArgs]:
+                    sensor = SensorData.from_args(sensorArgs)
+                case ["ESPInfo", *infoArgs]:
+                    info = ESPInfo.from_args(infoArgs)
+                case [*_]:
+                    logger.info(data_str.strip("\r\n"))
 
-                c1 = old_status == "closing valve"
-                c2 = data_sensors["status"] == "preinfusion"
-                c3 = data_sensors["status"] == "infusion"
-                c4 = data_sensors["status"] == "spring"
-                # logger.info(c1, end = "")
-                # logger.info(c2, end = "")
-                # logger.info(len(data_sensors["status"]), end = "")
+            if data is not None:
+                is_idle = data.status == MachineStatusEnum.IDLE
+                is_infusion = data.status == MachineStatusEnum.INFUSION
+                is_preinfusion = data.status == MachineStatusEnum.PREINFUSION
+                is_spring = data.status == MachineStatusEnum.SPRING
+                was_preparing = old_status == MachineStatusEnum.CLOSING_VALE
 
-                # time = time.time() - start_time
-                if ((c1 and c2) or (c1 and c3) or (c1 and c4)):
+                if (was_preparing and (is_preinfusion or is_infusion or is_spring)):
                     time_flag = True
-                    start_time = time.time()
-                    logger.info("start_time: {:.1f}".format(start_time))
-                if (data_sensors["status"] == "idle"):
+                    shot_start_time = time.time()
+                    logger.info("start_time: {:.1f}".format(shot_start_time))
+
+                if (is_idle):
                     time_flag = False
 
                 if (time_flag):
-                    data_sensors["time"] = time.time() - start_time
+                    data_sensors = data.clone_with_time(time.time() - shot_start_time)
                 else:
-                    data_sensors["time"] = 0
+                    data_sensors = data
+                old_status = data_sensors.status
+                infoReady = True
 
-                old_status = data_sensors["status"]
-                # logger.info(data_sensors["status"])
+            if sensor is not None:
+                sensor_sensors = sensor
+                infoReady = True
 
-                #else:
-                #    para cuando no usa formato de colores
-                
-            elif data_str.find("CCW") > -1:
-                ccw_function()
-            elif data_str.find("CW") > -1:
-                cw_function()
-            elif data_str.find("push") > -1:
-                encoder_push_function()
-            elif data_str.find("pu_d") > -1:
-                encoder_double_function()
-            elif data_str.find("elng") > -1:
-                encoder_long_function()
-            elif data_str.find("ta_d") > -1:
-                tare_double_function()
-            elif data_str.find("ta_l") > -1:
-                tare_long_function()
-            elif data_str.find("strt") > -1:
-                start_function()
+            if info is not None:
+                esp_info = info
+                infoReady = True
 
-            elif data_str_sensors[0] == 'Sensors':
-                if usaFormatoDeColores:
-                    try:
-                        sensor_values = data_str_sensors[1].split('\033[0m')
-                        data_sensor_temperatures["external_1"] = sensor_values[1].split('\033[1;31m')[0]
-                        data_sensor_temperatures["external_2"] = sensor_values[2].split('\033[1;32m')[0]
-                        data_sensor_temperatures["bar_up"] = sensor_values[3].split('\033[1;32m')[0]
-                        data_sensor_temperatures["bar_mid_up"] = sensor_values[4].split('\033[1;32m')[0]
-                        data_sensor_temperatures["bar_mid_down"] = sensor_values[5].split('\033[1;32m')[0]
-                        data_sensor_temperatures["bar_down"] = sensor_values[6].split('\033[1;32m')[0]
-                        data_sensor_temperatures["tube"] = sensor_values[7].split('\033[1;33m')[0]
-                        data_sensor_temperatures["valve"] = sensor_values[8].split('\033[1;34m')[0]
-                        data_sensor_actuators["motor_position"]=sensor_values[9].split('\033[1;34m')[0]
-                        data_sensor_actuators["motor_speed"]=sensor_values[10].split('\033[1;36m')[0]
-                        data_sensor_actuators["motor_power"]=sensor_values[11].split('\033[1;36m')[0]
-                        data_sensor_actuators["motor_current"]=sensor_values[12].split('\033[1;36m')[0]
-                        data_sensor_actuators["bandheater_power"]=sensor_values[13].split('\033[1;35m')[0]
-                        data_sensor_comunication["preassure_sensor"] = sensor_values[14].split('\033[1;35m')[0]
-                        data_sensor_comunication["adc_0"] = sensor_values[15].split('\033[1;35m')[0]
-                        data_sensor_comunication["adc_1"] = sensor_values[16].split('\033[1;35m')[0]
-                        data_sensor_comunication["adc_2"] = sensor_values[17].split('\033[1;35m')[0]
-                        data_sensor_comunication["adc_3"] = sensor_values[18].split('\n')[0]
-                    except:
-                        # pass
-                        logger.error("ESP did not send sensor values correctly")
-
-            elif data_str_sensors[0] == 'ESPInfo':
-                info_not_valid = False
-
-                try:
-                    software_info["firmwareV"] = data_str_sensors[1]
-                except:
-                    software_info["firmwareV"]  = "not found"
-                    logger.error("ESP did not send firmware version correctly\n")
-                    info_not_valid = True
-                
-                try:
-                    software_info["fanStatus"] = data_str_sensors[2]
-                except:
-                    logger.error("ESP did not send fanStatus correctly")
-                    info_not_valid = True
-
-                try:
-                    hardware_info["mainVoltage"] = data_str_sensors[3].strip('\r\n')
-                except:
-                    logger.error("ESP did not send main voltage value correctly")
-                    info_not_valid = True
-                
-                infoReady = not info_not_valid      #if the info received is valid, then fla info ready, else dont flag it
-
-# logger.info(data_str_sensors[0])
-# logger.info(data_str)
+            # FIXME this should be callback to the frontends in the future
+            if button_event is not None:
+                match (button_event.event):
+                    case ButtonEventEnum.ENCODER_CLOCKWISE:
+                        cw_function()
+                    case ButtonEventEnum.ENCODER_COUNTERCLOCKWISE:
+                        ccw_function()
+                    case ButtonEventEnum.ENCODER_PUSH:
+                        encoder_push_function()
+                    case ButtonEventEnum.ENCODER_DOUBLE:
+                        encoder_double_function()
+                    case ButtonEventEnum.ENCODER_LONG:
+                        encoder_long_function()
+                    case ButtonEventEnum.TARE_DUBLE:
+                        tare_double_function()
+                    case ButtonEventEnum.TARE_LONG:
+                        tare_long_function()
+                    case ButtonEventEnum.START:
+                        start_function()
     
 def data_treatment():
     global connection
@@ -578,56 +505,17 @@ async def live():
 
         if (reboot_flag): await sio.emit("MANUAL-REBOOT")
 
-        await sio.emit("status", {
-            "name": data_sensors["status"],
-            # "name" : "idle",
-            "sensors": {
-                "p": data_sensors["pressure"],
-                "f": data_sensors["flow"],
-                "w": data_sensors["weight"],
-                "t": data_sensors["temperature"],
-            },
-            "time": str(data_sensors["time"]),
-            "profile": data_sensors["profile"],
-            "source": lastJSON_source,
-        })
+        await sio.emit("status", {**data_sensors.to_sio(), "source": lastJSON_source,})
 
         if sendInfoToFront:
-            await sio.emit("sensors", {
-                "t_ext_1": data_sensor_temperatures["external_1"],
-                "t_ext_2": data_sensor_temperatures["external_2"],
-                "t_bar_up": data_sensor_temperatures["bar_up"],
-                "t_bar_mu": data_sensor_temperatures["bar_mid_up"],
-                "t_bar_md": data_sensor_temperatures["bar_mid_down"],
-                "t_bar_down": data_sensor_temperatures["bar_down"],
-                "t_tube": data_sensor_temperatures["tube"],
-                "t_valv": data_sensor_temperatures["valve"],
-            })
-            
-            await sio.emit("comunication", {
-                "p": data_sensor_comunication["preassure_sensor"],
-                "a_0": data_sensor_comunication["adc_0"],
-                "a_1": data_sensor_comunication["adc_1"],
-                "a_2": data_sensor_comunication["adc_2"],
-                "a_3": data_sensor_comunication["adc_3"]
-            })
-    
-            await sio.emit("actuators", {
-                "m_pos": data_sensor_actuators["motor_position"],
-                "m_spd": data_sensor_actuators["motor_speed"],
-                "m_pwr": data_sensor_actuators["motor_power"],
-                "m_cur": data_sensor_actuators["motor_current"],
-                "bh_pwr": data_sensor_actuators["bandheater_power"]
-            })
-    
-            await sio.emit("INFO", {
-                "name": software_info["name"],
-                "lcdV" : software_info["lcdV"],
-                "firmwareV" : software_info["firmwareV"],
-                "backendV" : software_info["backendV"],
-                "fanStatus": software_info["fanStatus"],
-                "mainVoltage": hardware_info["mainVoltage"],
-            })
+            logger.info("Sending info to front")
+            if sensor_sensors is not None:
+                await sio.emit("sensors", sensor_sensors.to_sio_temperatures())
+                await sio.emit("comunication", sensor_sensors.to_sio_communication())
+                await sio.emit("actuators", sensor_sensors.to_sio_actuators())
+            if esp_info is not None:
+                # FIXME change to lowercase info for consistency
+                await sio.emit("INFO", {**software_info, **esp_info.to_sio()})
         await sio.sleep(SAMPLE_TIME)
         i = i + 1
 
