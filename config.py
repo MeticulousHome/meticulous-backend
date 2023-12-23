9b56a05208d44f0177dc800904c3253d14557c21
Mimoja <mimoja@meticuloushome.com>
2023-12-23 13:56:06 +0100

    Add config manager to replace most hardcoded values in the future
    So far the backend is not persisting any data. Instead it is running completely
    on hardcoded values.
    This is no longer possible with the different WifiModes which are now no longer
    treated on system level but treated by the backend. Therefore the backend needs
    to be able to persist settings.
    We are doing so by writing to a yaml file which we can load from disk again.
    To be able to easily extend the config file we are also adding a default config
    which in return is able to be merged with the read config from disk.
    This allows us not only to add new settings flags without a lot of extra effort
    but also to recover in case of a corrupted config file which will then contain
    known-good default values. Further config sanity checks can be implemented in
    the future.
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ config.py
@@ -0,0 +1,97 @@
+from typing import Any
+import yaml
+import os
+from pathlib import Path
+from datetime import datetime
+from mergedeep import merge
+
+from log import MeticulousLogger
+logger = MeticulousLogger.getLogger(__name__)
+
+DefaultConfiguration_V1 = {
+    # Only needs to be incremented in case of incompatible restructurings
+    "version": 1,
+}
+
+class MeticulousConfigDict(dict):
+    """
+    A class that extends the functionality of a standard dictionary to support
+    reading from and writing to a YAML configuration file on the disk.
+
+    Attributes:
+        __path (Path): The file path for the configuration file.
+        __configError (bool): Flag to indicate if there's an error in the configuration.
+
+    Args:
+        path (str, optional): The path to the YAML configuration file. Defaults to "./config.yml".
+
+    Raises:
+        ValueError: If the provided file extension is not .yml or .yaml.
+
+    Methods:
+        load(): Loads the configuration from the file specified by __path. If the file doesn't exist,
+                it calls save() to create a new one. In case of a loading error, it backs up the
+                current file and sets __configError to True.
+
+        save(): Saves the current configuration to the file specified by __path. It creates the
+                directory if it doesn't exist. The configuration is saved in YAML format, and the
+                saved string is printed to the console for verification.
+
+    Example:
+        >>> default_config = { "key1" : "value1" }
+        >>> config_dict = MeticulousConfigDict("./my_config.yml", default_config)
+        >>> config_dict["new_key"] = "new_value"
+        >>> config_dict.save()
+    """
+
+    def __init__(self, path, default_dict={}) -> None:
+        super().__init__(default_dict)
+
+        self.__path = Path(path)
+        self.__configError = False
+
+        ext = self.__path.suffix
+        if ext not in [".yml", ".yaml"]:
+            raise ValueError(
+                f"Invalid Extension provided! YAML (yml / yaml) expected, {ext} found")
+
+        self.load()
+
+        logger.info("Config initialized")
+
+        cs = yaml.dump(
+            self.copy(),  default_flow_style=False, allow_unicode=True)
+        for line in cs.split('\n'):
+            logger.debug(f"CONF: {line}")
+    def hasError(self):
+        return self.__configError
+
+    def load(self):
+        if not Path(self.__path).exists():
+            self.save()
+            logger.info("Created new config")
+        else:
+            with open(self.__path, "r") as f:
+                try:
+                    disk_config = yaml.safe_load(f)
+                    disk_version = disk_config.get("version")
+                    if disk_version is not None and disk_version > self["version"]:
+                        logger.warning("Config on disk is newer than this software")
+                    merge(self, disk_config)
+                    logger.info("Successfully loaded config from disk")
+                    self.__configError = False
+                except Exception as e:
+                    logger.warning(f"Failed to load config: {e}")
+                    basename, extension = os.path.splitext(self.__path)
+                    backup_path = basename + "_broken_" +  datetime.now().strftime("%Y_%m_%d_%H_%M_%S") +extension
+                    os.rename(self.__path, backup_path)
+                    self.__configError = True
+                self.save()
+
+    def save(self):
+        Path(self.__path).parent.mkdir(parents=True, exist_ok=True)
+        with open(self.__path, "w") as f:
+            yaml.dump(
+                self.copy(), f, default_flow_style=False, allow_unicode=True)
+
+MeticulousConfig = MeticulousConfigDict("./config.yml", DefaultConfiguration_V1)
