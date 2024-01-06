b817a11d871e70a7d804731965b5580e4cf620fa
Mimoja <mimoja@meticuloushome.com>
2024-01-06 04:14:11 +0100

    api: Add Profile manager logic
    So far the dial app and the react dash app had their own profile storage.
    This lead to desyncronization issues and the inablity to edit the profile
    which was loaded on one of them on the other.
    To remidiate this we are now moving all profile logic into the backend.
    As a first start we add a super simple read/write/list endpoint which safes
    the profile to disk.
    Longterm this sounds like a great usecase of a database. But making the backend
    stateless is currently far out so we build the MVP solution first to be able to
    prototype and work from there.
    It is noteably that the backend _does not_ understand the profiles so far and
    is therefore unable to verify them at all.
    It also doesn't sent the profile to the ESP32 yet once it has been writen
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ api/profiles.py
@@ -0,0 +1,69 @@
+import tornado.ioloop
+import tornado.web
+import json
+from profile import ProfileManager
+from .base_handler import BaseHandler
+
+from log import MeticulousLogger
+logger = MeticulousLogger.getLogger(__name__)
+
+class ListHandler(BaseHandler):
+    def get(self):
+        profiles = ProfileManager.list_profiles()
+        response = []
+        for profile in profiles:
+            p = profiles[profile].copy()
+            if "stages" in p:
+                del p["stages"]
+            response.append(p)
+        self.write(json.dumps(response))
+
+class SaveProfileHandler(BaseHandler):
+    def post(self, save_id):
+        try:
+            data = json.loads(self.request.body)
+            (name, profile_id) = ProfileManager.save_profile(save_id, data)
+            self.write({"name": name, "uuid": profile_id})
+        except Exception as e:
+            self.set_status(400)
+            self.write(f"Error: Failed to save profile")
+            logger.warning("Failed to save profile:", exc_info=e, stack_info=True)
+
+class LoadProfileHandler(BaseHandler):
+    def get(self, profile_id):
+        data = ProfileManager.get_profile(profile_id)
+        if data:
+            profile = ProfileManager.load_profile(data)
+            self.write({"name": profile["name"], "uuid": profile["id"]})
+        else:
+            self.set_status(404)
+            self.write(f"No profile found with UUID: {profile_id}")
+    
+    def post(self, data):
+        try:
+            data = json.loads(self.request.body)
+            profile = ProfileManager.load_profile(data)
+            self.write({"name": profile["name"], "uuid": profile["id"]})
+        except Exception as e:
+            self.set_status(400)
+            self.write(f"Error: Failed to load temporary profile")
+            logger.warning("Failed to save profile:", exc_info=e, stack_info=True)
+
+
+class GetProfileHandler(BaseHandler):
+    def get(self, profile_id):
+        logger.info("Request for profile "+profile_id)
+        data = ProfileManager.get_profile(profile_id)
+        if data:
+            self.write(data)
+        else:
+            self.set_status(404)
+            self.write(f"No profile found with UUID: {profile_id}")
+
+PROFILE_HANDLER= [
+        (r"/profile/list", ListHandler),
+        (r"/profile/save", SaveProfileHandler),
+        (r"/profile/load", LoadProfileHandler),
+        (r"/profile/get/([0-9a-fA-F-]+)", GetProfileHandler),
+]
+
