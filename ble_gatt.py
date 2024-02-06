1027d724d150afbf0cbc869516c4ea2eecbfe9c0
Mimoja <mimoja@meticuloushome.com>
2024-02-06 14:22:16 +0100

    gatt: Format file using black autoformater
    The Gatt server code was one of the first new components to the backend
    in a long while and was not op-to-spec when it comes to formating.
    Correcting now
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- ble_gatt.py
+++ ble_gatt.py
@@ -10,20 +10,22 @@ from bless import (  # type: ignore
     BlessServer,
     BlessGATTCharacteristic,
     GATTCharacteristicProperties,
-    GATTAttributePermissions
+    GATTAttributePermissions,
 )
 
 from wifi import WifiManager
 from config import *
 
 from log import MeticulousLogger
+
 logger = MeticulousLogger.getLogger(__name__)
 
 # NOTE: Some systems require different synchronization methods.
 if sys.platform in ["darwin", "win32"]:
     raise ValueError("Cannot run on non-linux platforms")
 
-class GATTServer():
+
+class GATTServer:
     """
     A class representing a BLE (Bluetooth Low Energy) GATT (Generic Attribute Profile) Server
     for Wi-Fi provisioning purposes. This server allows a BLE client to interact with the server
@@ -73,15 +75,15 @@ class GATTServer():
         self.loop = asyncio.new_event_loop()
 
         def _exception_handler(loop, context):
-            logger.exception("GATT Server crashed!",
-                             exc_info=context, stack_info=True)
+            logger.exception("GATT Server crashed!", exc_info=context, stack_info=True)
             GATTServer.getServer().stop()
+
         self.loop.set_exception_handler(_exception_handler)
         self.improv_server = ImprovProtocol(
             requires_authorization=False,
             wifi_connect_callback=GATTServer.wifi_connect,
             wifi_networks_callback=GATTServer.get_wifi_networks,
-            max_response_bytes=250
+            max_response_bytes=250,
         )
         self.gatt_name = MeticulousConfig[CONFIG_SYSTEM][GATT_NAME]
         self.bless_gatt_server = None
@@ -92,14 +94,12 @@ class GATTServer():
             GATTServer._singletonServer = GATTServer()
         return GATTServer._singletonServer
 
-    def allow_wifi_provisioning():
-        server = GATTServer.getServer().improv_server
-        if server.state == ImprovState.AWAITING_AUTHORIZATION:
-            server.state = ImprovState.AUTHORIZED
+    def allow_wifi_provisioning(self):
+        if self.improv_server.state == ImprovState.AWAITING_AUTHORIZATION:
+            self.improv_server.state = ImprovState.AUTHORIZED
 
-    def is_provisioning():
-        server = GATTServer.getServer().improv_server
-        return server.state.value >= ImprovState.AUTHORIZED.value
+    def is_provisioning(self):
+        return self.improv_server.state.value >= ImprovState.AUTHORIZED.value
 
     def is_running(self):
         if self.loopThread is None:
@@ -148,8 +148,8 @@ class GATTServer():
 
         # Power on the hci device if it is powered off
         await self.bless_gatt_server.setup_task
-        interface = self.bless_gatt_server.adapter.get_interface(
-            'org.bluez.Adapter1')
+
+        interface = self.bless_gatt_server.adapter.get_interface("org.bluez.Adapter1")
         powered = await interface.get_powered()
         if not powered:
             logger.info("bluetooth device is not powered, powering now!")
@@ -172,8 +172,7 @@ class GATTServer():
         except KeyboardInterrupt:
             logger.info("KeyboardInterrupt: Shutting Down")
         except Exception as e:
-            logger.exception("GATT Server Crashed",
-                             exc_info=e, stack_info=True)
+            logger.exception("GATT Server Crashed", exc_info=e, stack_info=True)
             raise
         logger.info("GATT Server stopped")
 
@@ -181,48 +180,59 @@ class GATTServer():
         gatt: Dict = {
             ImprovUUID.SERVICE_UUID.value: {
                 ImprovUUID.STATUS_UUID.value: {
-                    "Properties": (GATTCharacteristicProperties.read |
-                                   GATTCharacteristicProperties.notify),
-                    "Permissions": (GATTAttributePermissions.readable |
-                                    GATTAttributePermissions.writeable)
+                    "Properties": (
+                        GATTCharacteristicProperties.read
+                        | GATTCharacteristicProperties.notify
+                    ),
+                    "Permissions": (
+                        GATTAttributePermissions.readable
+                        | GATTAttributePermissions.writeable
+                    ),
                 },
                 ImprovUUID.ERROR_UUID.value: {
-                    "Properties": (GATTCharacteristicProperties.read |
-                                   GATTCharacteristicProperties.notify),
-                    "Permissions": (GATTAttributePermissions.readable |
-                                    GATTAttributePermissions.writeable)
+                    "Properties": (
+                        GATTCharacteristicProperties.read
+                        | GATTCharacteristicProperties.notify
+                    ),
+                    "Permissions": (
+                        GATTAttributePermissions.readable
+                        | GATTAttributePermissions.writeable
+                    ),
                 },
                 ImprovUUID.RPC_COMMAND_UUID.value: {
-                    "Properties": (GATTCharacteristicProperties.read |
-                                   GATTCharacteristicProperties.write |
-                                   GATTCharacteristicProperties.write_without_response),
-                    "Permissions": (GATTAttributePermissions.readable |
-                                    GATTAttributePermissions.writeable)
+                    "Properties": (
+                        GATTCharacteristicProperties.read
+                        | GATTCharacteristicProperties.write
+                        | GATTCharacteristicProperties.write_without_response
+                    ),
+                    "Permissions": (
+                        GATTAttributePermissions.readable
+                        | GATTAttributePermissions.writeable
+                    ),
                 },
                 ImprovUUID.RPC_RESULT_UUID.value: {
-                    "Properties": (GATTCharacteristicProperties.read |
-                                   GATTCharacteristicProperties.notify),
-                    "Permissions": (GATTAttributePermissions.readable)
+                    "Properties": (
+                        GATTCharacteristicProperties.read
+                        | GATTCharacteristicProperties.notify
+                    ),
+                    "Permissions": (GATTAttributePermissions.readable),
                 },
                 ImprovUUID.CAPABILITIES_UUID.value: {
                     "Properties": (GATTCharacteristicProperties.read),
-                    "Permissions": (GATTAttributePermissions.readable)
+                    "Permissions": (GATTAttributePermissions.readable),
                 },
             }
         }
         return gatt
 
     def wifi_connect(ssid: str, passwd: str) -> Optional[list[str]]:
-        ssid = ssid.decode('utf-8')
-        passwd = passwd.decode('utf-8')
-        logger.info(
-            f"Connecting to '{ssid}' with password: '{passwd}'")
+        ssid = ssid.decode("utf-8")
+        passwd = passwd.decode("utf-8")
+        logger.info(f"Connecting to '{ssid}' with password: '{passwd}'")
         if WifiManager.connectToWifi(ssid, passwd):
             networkConfig = WifiManager.getCurrentConfig()
-            localServer = [
-                f"http://{str(localIP.ip)}" for localIP in networkConfig.ips]
-            logger.debug(
-                f"Backend redirect IP/URL: {localServer}")
+            localServer = [f"http://{str(localIP.ip)}" for localIP in networkConfig.ips]
+            logger.debug(f"Backend redirect IP/URL: {localServer}")
             return localServer
         return None
 
@@ -232,10 +242,7 @@ class GATTServer():
             ssids.append(s.ssid)
         return ssids
 
-    def read_request(
-            characteristic: BlessGATTCharacteristic,
-            **kwargs
-    ) -> bytearray:
+    def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
         try:
             improv_char = ImprovUUID(characteristic.uuid)
             logger.info(f"Reading {improv_char} : {characteristic}")
@@ -243,28 +250,33 @@ class GATTServer():
             logger.info(f"Reading {characteristic.uuid}")
             pass
         if characteristic.service_uuid == ImprovUUID.SERVICE_UUID.value:
-            return GATTServer.getServer().improv_server.handle_read(characteristic.uuid)
+            value = GATTServer.getServer().improv_server.handle_read(
+                characteristic.uuid
+            )
+            GATTServer.getServer().allow_wifi_provisioning()
+            return value
         return characteristic.value
 
     def write_request(
-        characteristic: BlessGATTCharacteristic,
-        value: bytearray,
-        **kwargs
+        characteristic: BlessGATTCharacteristic, value: bytearray, **kwargs
     ):
         if characteristic.service_uuid == ImprovUUID.SERVICE_UUID.value:
-            (target_uuid, target_values) = GATTServer.getServer().improv_server.handle_write(
-                characteristic.uuid, value)
+            (
+                target_uuid,
+                target_values,
+            ) = GATTServer.getServer().improv_server.handle_write(
+                characteristic.uuid, value
+            )
             if target_uuid != None and target_values != None:
                 for value in target_values:
-                    logger.debug(
-                        f"Setting {ImprovUUID(target_uuid)} to {value}")
+                    logger.debug(f"Setting {ImprovUUID(target_uuid)} to {value}")
                     GATTServer.getServer().bless_gatt_server.get_characteristic(
                         target_uuid,
                     ).value = value
                     success = GATTServer.getServer().bless_gatt_server.update_value(
-                        ImprovUUID.SERVICE_UUID.value,
-                        target_uuid
+                        ImprovUUID.SERVICE_UUID.value, target_uuid
                     )
                     if not success:
                         logger.warning(
-                            f"Updating characteristic return status={success}")
+                            f"Updating characteristic return status={success}"
+                        )
