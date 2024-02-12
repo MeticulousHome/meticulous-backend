1b80481d206e154eebe819016398b70bba4efc88
AUOR17 <angel_osorno@outlook.com>
2024-02-12 11:56:27 -0600

    feat(ProfileManager): controllers and dictionaries
    - Each control has its own class
    - Some dictionary keys have been converted into ENUM type variables to prevent typing errors and make access and maintenance easier

--- profile_manager/controllers.py
+++ profile_manager/controllers.py
@@ -1,7 +1,10 @@
 import json 
-from dictionaries import controllers_type, algorithms_type, reference_type, curve_interpolation
-from dictionaries import ReferenceType, Pressure_Algorithm_Type, Temperature_Algorithm_Type, Speed_Algorithm_Type
+from dictionaries import controllers_type, algorithms_type, reference_type, curve_interpolation, messages, directions
+from dictionaries import ReferenceType, Pressure_Algorithm_Type, Temperature_Algorithm_Type, Speed_Algorithm_Type, Message_Type, CurveInterpolationType, Direction_Type
+
 class Controllers:
+#This parent class has the get_controller method that returns a dictionary with the information of the controllers
+
     def __init__(self):
         self.data = {}
         
@@ -9,6 +12,19 @@ class Controllers:
         return self.data
 
 class CurveControllers(Controllers):
+    
+    '''
+    This child class of Controllers is a special class made for controllers that have an associated curve.
+
+    Attributes:
+
+    set_curve_id: int -> ID of the curve associated with the controller
+    set_interpolation_kind: str -> type of curve interpolation
+    set_points: list -> list of points of the curve
+    set_reference_type: str -> type of curve reference
+    set_reference_id: int -> ID of the curve reference
+    self.data: dict -> dictionary with the information of the controller and its associated curve
+    ''' 
     def __init__(self):
         self.data = {
             "kind": "",
@@ -23,8 +39,6 @@ class CurveControllers(Controllers):
                 }
             }
         }
-        self.algorithm_dictionary = []
-        self.reference_diccionary = []
     
     def set_curve_id(self, id: int):
         self.data["curve"]["id"] = id
@@ -46,12 +60,17 @@ class CurveControllers(Controllers):
     def set_reference_id(self, reference_id: int):
         self.data["curve"]["reference"]["id"] = reference_id
 
+'''
+Child classes of CurveControllers that represent pressure, flow, temperature, power, and weight controllers
+These classes have methods to change the controller's algorithm and to change the curve associated with the controller
+'''
 class pressure_controller(CurveControllers):
     def __init__(self):
+        super().__init__()
         self.data["kind"] = controllers_type["pressure"]
-        self.data["algorithm"] = algorithms_type["pressure"]["pid v1"]
+        self.data["algorithm"] = algorithms_type["pressure"][Pressure_Algorithm_Type.PID_V1]
         self.data["curve"]["id"] = 0
-        self.data["curve"]["interpolation_kind"] = curve_interpolation["linear"]
+        self.data["curve"]["interpolation_kind"] = curve_interpolation[CurveInterpolationType.LINEAR]
         self.data["curve"]["points"] = [0,6]
         self.data["curve"]["reference"]["kind"] = reference_type["curve"][ReferenceType.TIME]
         self.data["curve"]["reference"]["id"] = 0
@@ -67,13 +86,15 @@ class pressure_controller(CurveControllers):
 
 class flow_controller(CurveControllers):
     def __init__(self):
+        super().__init__()
         self.data["kind"] = controllers_type["flow"]
         self.data["algorithm"] = algorithms_type["flow"]
         
 class temperature_controller(CurveControllers):
     def __init__(self):
+        super().__init__()
         self.data["kind"] = controllers_type["temperature"]
-        self.data["algorithm"] = algorithms_type["temperature"]["water"]
+        self.data["algorithm"] = algorithms_type["temperature"][Temperature_Algorithm_Type.WATER]
         
     def set_algorithm(self, algorithm: Temperature_Algorithm_Type):
         if algorithm not in algorithms_type["temperature"]:
@@ -83,6 +104,7 @@ class temperature_controller(CurveControllers):
         
 class speed_controller(Controllers):
     def __init__(self):
+        super().__init__()
         self.data = {
             "kind": controllers_type["speed"],
             "algorithm": algorithms_type["speed"][Speed_Algorithm_Type.EASE_IN],
@@ -95,176 +117,119 @@ class speed_controller(Controllers):
             raise ValueError("Invalid algorithm")
         
         self.data["algorithm"] = algorithms_type["speed"][algorithm]
+    
+    def set_speed(self, speed: int):
+        self.data["speed"] = speed
+    
+    def set_direction(self, direction: Direction_Type):
+        if direction not in directions:
+            raise ValueError("Invalid direction")
         
-    
-    
-    
-    def pressure_controller(self, curve_id: int, interpolation_kind: str, points: list, reference_kind: str, reference_id: int):
-        self.data = {
-            "kind": self.controllers_type["pressure"],
-            "algorithm": self.algorithms_type["pressure"]["pid v1"],
-            "curve" : {
-                "id" : curve_id,
-                "interpolation_kind": self.curve_interpolation[interpolation_kind],
-                "points" : points,
-                "reference" :{
-                    "kind" : self.reference_type["kind"][reference_kind],
-                    "id" : reference_id
-                }
-            }
-        }
-        return self.data
-    
-    def flow_controller(self, curve_id: int, interpolation_kind: str, points: list, reference_kind: str, reference_id: int):
-        self.data = {
-            "kind": self.controllers_type["flow"],
-            "algorithm": self.algorithms_type["flow"],
-            "curve" : {
-                "id" : curve_id,
-                "interpolation_kind" : self.curve_interpolation[interpolation_kind],
-                "points" : points,
-                "reference" :{
-                    "kind" : self.reference_type["kind"][reference_kind],
-                    "id" : reference_id
-                }
-            }
-        }
-        return self.data
+        self.data["direction"] = directions[direction]
         
-    def power_controller(self, curve_id: int, interpolation_kind: str, points: list, reference_kind: str, reference_id: int):
+class power_controller(CurveControllers):
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = controllers_type["power"]
+        self.data["algorithm"] = algorithms_type["power"]
+
+class weight_controller(CurveControllers):
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = controllers_type["weight"]
+        self.data["algorithm"] = algorithms_type["weight"]
+
+class log_controller(Controllers):
+    #This class displays a message
+    def __init__(self):
         self.data = {
-            "kind": self.controllers_type["power"],
-            "algorithm": self.algorithms_type["power"],
-            "curve" : {
-                "id" : curve_id,
-                "interpolation_kind" : self.curve_interpolation[interpolation_kind],
-                "points" : points,
-                "reference" :{
-                    "kind" : self.reference_type["kind"][reference_kind],
-                    "id" : reference_id
-                }
-            }
+            "kind": controllers_type["message"],
+            "message": ""
         }
-        return self.data
-    
-    def weight_controller(self, curve_id: int, interpolation_kind: str, points: list, reference_kind: str, reference_id: int):
+        
+    def set_message(self, message: Message_Type):
+        if message not in messages:
+            raise ValueError("Invalid message")
+        
+        self.data["message"] = messages[message]
+
+class tare_controller(Controllers):
+    #This class is a controller that when called, the machine makes a tare
+    def __init__(self):
         self.data = {
-            "kind": self.controllers_type["weight"],
-            "algorithm": self.algorithms_type["weight"],
-            "curve" : {
-                "id" : curve_id,
-                "interpolation_kind" : self.curve_interpolation[interpolation_kind],
-                "points" : points,
-                "reference" :{
-                    "kind" : self.reference_type["kind"][reference_kind],
-                    "id" : reference_id
-                }
-            }
+            "kind": controllers_type["tare"]
         }
-        return self.data
     
-    def temperature_controller(self, curve_id: int, interpolation_kind: str, points: list, reference_kind: str, reference_id: int, algorithm: str):
+class end_profile(Controllers):
+    #This class is a controller that when called, the machine finishes the profile
+    def __init__(self):
         self.data = {
-            "kind": self.controllers_type["temperature"],
-            "algorithm": self.algorithms_type["temperature"][algorithm],
-            "curve" : {
-                "id" : curve_id,
-                "interpolation_kind" : self.curve_interpolation[interpolation_kind],
-                "points" : points,
-                "reference" :{
-                    "kind" : self.reference_type["kind"][reference_kind],
-                    "id" : reference_id
-                }
-            }
+            "kind": controllers_type["end"]
         }
-        return self.data
-    
-    def speed_controller(self, speed_value: int, algorithm: str, direction: str):
-        self.data = {
-            "kind": self.controllers_type["speed"],
-            "algorithm": self.algorithms_type["speed"][algorithm],
-            "speed" : speed_value,
-            "direction" : self.direction[direction]
-        }
-        return self.data
-    
-    def log_controller(self, message: str):
-        self.data = {
-            "kind": self.controllers_type["message"],
-            "message": self.messages[message]
-        }
-        return self.data
-    
-    def tare_controller(self):
-        self.data = {
-            "kind": self.controllers_type["tare"]
-        }
-        return self.data
-    
-    def end_profile(self):
-        self.data = {
-            "kind": self.controllers_type["end"]
-        }
-        return self.data
-    
-    def build_reference(self, id: int, reference_kind: str):
-        self.data = {
-            "kind": self.reference_type["reference"][reference_kind],
-            "id": id
-        }
-        return self.data
-    
-    
-    
-    
+        
     
 if __name__ == "__main__":
     # Example usage of the Controllers class.
     
-    """
-    Controllers Class Documentation:
+    # All the controllers are initialized with default values    
 
-    The Controllers class is a powerful tool for converting simple JSON into more complex JSON structures, 
-    specifically designed for handling various types of controls within the application. 
-    Here's a breakdown of what this class offers and how to use it effectively:
-
-    Key Features:
-    - This class is your go-to for creating controls such as pressure, flow, power, weight, and temperature. 
-    Each control type is supported by a set of key features including:
-        Algorithm, Curve ID, Interpolation kind, Points, Reference type, and Reference ID.
-    - For each type of control, there's a dedicated method in this class. These methods are designed to take arguments like:
-    curve ID, interpolation kind, points, reference kind, and reference ID. 
-    - The temperature control stands out because it requires an additional algorithm argument.
-
-    Handling References:
-    - In the realm of complex JSON, references are treated uniquely. 
-    The `build_reference` method in this class allows to build these references by specifying the reference ID and the type
-    it belongs to. Currently, this includes types such as Time, Position, and Weight.
-
-    Special Controls:
-    - The class also introduces special controls for specific needs:
-      - Speed control combines control type, algorithm, and specifics like speed and direction.
-      - Tare control simplifies the process of calling the "tare" function on a scale.
-      - The "end_profile" control signals the completion of a profile.
-      - Lastly, the "log" control is designed for message logging, ensuring you can easily display messages as needed.
-
-    """
-
-    controllers = Controllers()
-    points = [
-        [0, 1],
-        [10, 2],
-        [30, 3]
-    ]
-    print(json.dumps(controllers.pressure_controller(1, "linear", points, "time", 10), indent=4))
-    print(json.dumps(controllers.flow_controller(1, "linear", points, "time", 10), indent=4))
-    print(json.dumps(controllers.power_controller(1, "linear", points, "time", 10), indent=4))
-    print(json.dumps(controllers.weight_controller(1, "linear", points, "time", 10), indent=4))
-    print(json.dumps(controllers.temperature_controller(1, "linear", points, "time", 10, "water"), indent=4))
-    print(json.dumps(controllers.speed_controller(10, "ease-in", "forward"), indent=4))
-    print(json.dumps(controllers.log_controller("no water"), indent=4))
-    print(json.dumps(controllers.tare_controller(), indent=4))
-    print(json.dumps(controllers.end_profile(), indent=4))
-    print(json.dumps(controllers.build_reference(1, "time"), indent=4))
-    print(json.dumps(controllers.build_reference(1, "weight"), indent=4))
-    print(json.dumps(controllers.build_reference(1, "position"), indent=4))
+    points = [[0, 6],[10,8]]
+    
+    pressure_controller_1 = pressure_controller()
+    pressure_controller_1.set_algorithm(Pressure_Algorithm_Type.PID_V1)
+    pressure_controller_1.set_curve_id(1)
+    pressure_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    pressure_controller_1.set_points(points)
+    pressure_controller_1.set_reference_type(ReferenceType.TIME)
+    pressure_controller_1.set_reference_id(2)
+    print(json.dumps(pressure_controller_1.get_controller(), indent=4))
+    
+    flow_controller_1 = flow_controller()
+    flow_controller_1.set_curve_id(3)
+    flow_controller_1.set_interpolation_kind(CurveInterpolationType.CATMULL)
+    flow_controller_1.set_points(points)
+    flow_controller_1.set_reference_type(ReferenceType.POSITION)
+    flow_controller_1.set_reference_id(4)
+    print(json.dumps(flow_controller_1.get_controller(), indent=4))
+    
+    temperature_controller_1 = temperature_controller()
+    temperature_controller_1.set_algorithm(Temperature_Algorithm_Type.WATER)
+    temperature_controller_1.set_curve_id(5)
+    temperature_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    temperature_controller_1.set_points(points)
+    temperature_controller_1.set_reference_type(ReferenceType.WEIGHT)
+    temperature_controller_1.set_reference_id(6)
+    print(json.dumps(temperature_controller_1.get_controller(), indent=4))
+    
+    speed_controller_1 = speed_controller()
+    speed_controller_1.set_algorithm(Speed_Algorithm_Type.EASE_IN)
+    speed_controller_1.set_speed(7)
+    speed_controller_1.set_direction(Direction_Type.FORWARD)
+    print(json.dumps(speed_controller_1.get_controller(), indent=4))
+    
+    power_controller_1 = power_controller()
+    power_controller_1.set_curve_id(7)
+    power_controller_1.set_interpolation_kind(CurveInterpolationType.CATMULL)
+    power_controller_1.set_points(points)
+    power_controller_1.set_reference_type(ReferenceType.TIME)
+    power_controller_1.set_reference_id(8)
+    print(json.dumps(power_controller_1.get_controller(), indent=4))
+    
+    weight_controller_1 = weight_controller()
+    weight_controller_1.set_curve_id(9)
+    weight_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    weight_controller_1.set_points(points)
+    weight_controller_1.set_reference_type(ReferenceType.POSITION)
+    weight_controller_1.set_reference_id(10)
+    print(json.dumps(weight_controller_1.get_controller(), indent=4))
+    
+    log_controller_1 = log_controller()
+    log_controller_1.set_message(Message_Type.NO_WATER) 
+    print(json.dumps(log_controller_1.get_controller(), indent=4))
+    
+    tare_controller_1 = tare_controller()
+    print(json.dumps(tare_controller_1.get_controller(), indent=4))
+    
+    end_profile_1 = end_profile()
+    print(json.dumps(end_profile_1.get_controller(), indent=4))
+    
