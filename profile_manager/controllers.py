2adb58dbf868df992a8d24d5bcc58051e5b03aec
AUOR17 <angel_osorno@outlook.com>
2024-02-14 15:23:50 -0600

    Class Nodes created to add controllers and trigger
    - The enum file was created to store the classes according to the controllers an triggers.
    - The dictionaries file was modified to only store the dictionaries of controllers an triggers.
    This file was modified and now imports the enums file.
    - The controllers file was modified according to the new dictionaries and enums files.
    - The triggers file was modified according to the new dictionaries and enums files.

--- profile_manager/controllers.py
+++ profile_manager/controllers.py
@@ -1,7 +1,6 @@
 import json 
-from dictionaries import controllers_type, algorithms_type, reference_type, curve_interpolation, messages, directions
-from dictionaries import ReferenceType, Pressure_Algorithm_Type, Temperature_Algorithm_Type, Speed_Algorithm_Type, Message_Type, CurveInterpolationType, Direction_Type
-
+import dictionaries as dic
+from dictionaries import controllers_type, algorithms_type, curve_interpolation, reference_type, directions, messages
 class Controllers:
 #This parent class has the get_controller method that returns a dictionary with the information of the controllers
 
@@ -43,7 +42,7 @@ class CurveControllers(Controllers):
     def set_curve_id(self, id: int):
         self.data["curve"]["id"] = id
 
-    def set_interpolation_kind(self, interpolation_kind: str):
+    def set_interpolation_kind(self, interpolation_kind: dic.enums.CurveInterpolationType):
         if interpolation_kind not in curve_interpolation:
             raise ValueError("Invalid interpolation kind")
         
@@ -52,10 +51,10 @@ class CurveControllers(Controllers):
     def set_points(self, points: list):
         self.data["curve"]["points"] = points
         
-    def set_reference_type(self, reference_kind: ReferenceType):
-        if reference_kind not in reference_type["curve"]:
+    def set_reference_type(self, reference_kind: dic.enums.ReferenceType):
+        if reference_kind not in reference_type[dic.enums.ReferenceType.CURVE]:
             raise ValueError("Invalid reference kind")
-        self.data["curve"]["reference"]["kind"] = reference_type["curve"][reference_kind]
+        self.data["curve"]["reference"]["kind"] = reference_type[dic.enums.ReferenceType.CURVE][reference_kind]
         
     def set_reference_id(self, reference_id: int):
         self.data["curve"]["reference"]["id"] = reference_id
@@ -64,108 +63,154 @@ class CurveControllers(Controllers):
 Child classes of CurveControllers that represent pressure, flow, temperature, power, and weight controllers
 These classes have methods to change the controller's algorithm and to change the curve associated with the controller
 '''
-class pressure_controller(CurveControllers):
+class PressureController(CurveControllers):
     def __init__(self):
         super().__init__()
-        self.data["kind"] = controllers_type["pressure"]
-        self.data["algorithm"] = algorithms_type["pressure"][Pressure_Algorithm_Type.PID_V1]
+        self.data["kind"] = controllers_type[dic.enums.ControllerType.PRESSURE]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.PRESSURE][dic.enums.Pressure_Algorithm_Type.PID_V1]
         self.data["curve"]["id"] = 0
-        self.data["curve"]["interpolation_kind"] = curve_interpolation[CurveInterpolationType.LINEAR]
+        self.data["curve"]["interpolation_kind"] = curve_interpolation[dic.enums.CurveInterpolationType.LINEAR]
         self.data["curve"]["points"] = [0,6]
-        self.data["curve"]["reference"]["kind"] = reference_type["curve"][ReferenceType.TIME]
+        self.data["curve"]["reference"]["kind"] = reference_type[dic.enums.ReferenceType.CURVE][dic.enums.ReferenceType.TIME]
         self.data["curve"]["reference"]["id"] = 0
         
     
-    def set_algorithm(self, algorithm: Pressure_Algorithm_Type):
+    def set_algorithm(self, algorithm: dic.enums.Pressure_Algorithm_Type):
     # only accept valid algorithms
-        if algorithm not in algorithms_type["pressure"]:
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.PRESSURE]:
             raise ValueError("Invalid algorithm")
 
-        self.data["algorithm"] = algorithms_type["pressure"][algorithm]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.PRESSURE][algorithm]
 
 
-class flow_controller(CurveControllers):
+class FlowController(CurveControllers):
     def __init__(self):
         super().__init__()
-        self.data["kind"] = controllers_type["flow"]
-        self.data["algorithm"] = algorithms_type["flow"]
+        self.data["kind"] = controllers_type[dic.enums.ControllerType.FLOW]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.FLOW][dic.enums.Flow_Algorithm_Type.PID_V1]
         
-class temperature_controller(CurveControllers):
-    def __init__(self):
-        super().__init__()
-        self.data["kind"] = controllers_type["temperature"]
-        self.data["algorithm"] = algorithms_type["temperature"][Temperature_Algorithm_Type.WATER]
-        
-    def set_algorithm(self, algorithm: Temperature_Algorithm_Type):
-        if algorithm not in algorithms_type["temperature"]:
+    def set_algorithm(self, algorithm: dic.enums.Flow_Algorithm_Type):
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.FLOW]:
             raise ValueError("Invalid algorithm")
         
-        self.data["algorithm"] = algorithms_type["temperature"][algorithm]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.FLOW][algorithm]
         
-class speed_controller(Controllers):
+class TemperatureController(CurveControllers):
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = controllers_type[dic.enums.ControllerType.TEMPERATURE]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.TEMPERATURE][dic.enums.Temperature_Algorithm_Type.WATER]
+        
+    def set_algorithm(self, algorithm: dic.enums.Temperature_Algorithm_Type):
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.TEMPERATURE]:
+            raise ValueError("Invalid algorithm")
+        
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.TEMPERATURE][algorithm]
+        
+class SpeedController(Controllers):
     def __init__(self):
         super().__init__()
         self.data = {
-            "kind": controllers_type["speed"],
-            "algorithm": algorithms_type["speed"][Speed_Algorithm_Type.EASE_IN],
+            "kind": controllers_type[dic.enums.ControllerType.SPEED],
+            "algorithm": algorithms_type[dic.enums.AlgorithmType.SPEED][dic.enums.Speed_Algorithm_Type.EASE_IN],
             "speed" : 0,
             "direction" : ""
         }
         
-    def set_algorithm(self, algorithm: Speed_Algorithm_Type):
-        if algorithm not in algorithms_type["speed"]:
+    def set_algorithm(self, algorithm: dic.enums.Speed_Algorithm_Type):
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.SPEED]:
             raise ValueError("Invalid algorithm")
         
-        self.data["algorithm"] = algorithms_type["speed"][algorithm]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.SPEED][algorithm]
     
     def set_speed(self, speed: int):
         self.data["speed"] = speed
     
-    def set_direction(self, direction: Direction_Type):
+    def set_direction(self, direction: dic.enums.Direction_Type):
         if direction not in directions:
             raise ValueError("Invalid direction")
         
         self.data["direction"] = directions[direction]
         
-class power_controller(CurveControllers):
+class PowerController(CurveControllers):
     def __init__(self):
         super().__init__()
-        self.data["kind"] = controllers_type["power"]
-        self.data["algorithm"] = algorithms_type["power"]
+        self.data["kind"] = controllers_type[dic.enums.ControllerType.POWER]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.POWER][dic.enums.Power_Algorithm_Type.SPRING]
+    
+    def set_algorithm(self, algorithm: dic.enums.Power_Algorithm_Type):
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.POWER]:
+            raise ValueError("Invalid algorithm")
+        
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.POWER][algorithm]
 
-class weight_controller(CurveControllers):
+class WeightController(CurveControllers):
     def __init__(self):
         super().__init__()
-        self.data["kind"] = controllers_type["weight"]
-        self.data["algorithm"] = algorithms_type["weight"]
+        self.data["kind"] = controllers_type[dic.enums.ControllerType.WEIGHT]
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.WEIGHT][dic.enums.Weight_Algorithm_Type.PID_V1]
+        
+    def set_algorithm(self, algorithm: dic.enums.Weight_Algorithm_Type):
+        if algorithm not in algorithms_type[dic.enums.AlgorithmType.WEIGHT]:
+            raise ValueError("Invalid algorithm")
+        
+        self.data["algorithm"] = algorithms_type[dic.enums.AlgorithmType.WEIGHT][algorithm]
 
-class log_controller(Controllers):
+class LogController(Controllers):
     #This class displays a message
     def __init__(self):
         self.data = {
-            "kind": controllers_type["message"],
+            "kind": controllers_type[dic.enums.ControllerType.MESSAGE],
             "message": ""
         }
         
-    def set_message(self, message: Message_Type):
+    def set_message(self, message: dic.enums.Message_Type):
         if message not in messages:
             raise ValueError("Invalid message")
         
         self.data["message"] = messages[message]
 
-class tare_controller(Controllers):
+class TareController(Controllers):
     #This class is a controller that when called, the machine makes a tare
     def __init__(self):
         self.data = {
-            "kind": controllers_type["tare"]
+            "kind": controllers_type[dic.enums.ControllerType.TARE]
         }
     
-class end_profile(Controllers):
+class EndProfile(Controllers):
     #This class is a controller that when called, the machine finishes the profile
     def __init__(self):
         self.data = {
-            "kind": controllers_type["end"]
+            "kind": controllers_type[dic.enums.ControllerType.END]
         }
+        
+class ReferenceController(Controllers):
+    #This class is a controller that when called, the machine makes a reference
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = reference_type[dic.enums.ReferenceType.CONTROL][dic.enums.ReferenceType.TIME]
+        self.data["id"] = 0
+        
+    def set_reference_id(self, id: int):
+        self.data["id"] = id
+        
+class TimeReferenceController(ReferenceController):
+    #This class is a controller that when called, the machine makes a reference in time
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = reference_type[dic.enums.ReferenceType.CONTROL][dic.enums.ReferenceType.TIME]
+        
+class PositionReferenceController(ReferenceController):
+    #This class is a controller that when called, the machine makes a reference in position
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = reference_type[dic.enums.ReferenceType.CONTROL][dic.enums.ReferenceType.POSITION]
+        
+class WeightReferenceController(ReferenceController):
+    #This class is a controller that when called, the machine makes a reference in weight
+    def __init__(self):
+        super().__init__()
+        self.data["kind"] = reference_type[dic.enums.ReferenceType.CONTROL][dic.enums.ReferenceType.WEIGHT]   
         
     
 if __name__ == "__main__":
@@ -175,61 +220,73 @@ if __name__ == "__main__":
 
     points = [[0, 6],[10,8]]
     
-    pressure_controller_1 = pressure_controller()
-    pressure_controller_1.set_algorithm(Pressure_Algorithm_Type.PID_V1)
+    pressure_controller_1 = PressureController()
+    pressure_controller_1.set_algorithm(dic.enums.Pressure_Algorithm_Type.PID_V1)
     pressure_controller_1.set_curve_id(1)
-    pressure_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    pressure_controller_1.set_interpolation_kind(dic.enums.CurveInterpolationType.LINEAR)
     pressure_controller_1.set_points(points)
-    pressure_controller_1.set_reference_type(ReferenceType.TIME)
+    pressure_controller_1.set_reference_type(dic.enums.ReferenceType.TIME)
     pressure_controller_1.set_reference_id(2)
     print(json.dumps(pressure_controller_1.get_controller(), indent=4))
     
-    flow_controller_1 = flow_controller()
+    flow_controller_1 = FlowController()
     flow_controller_1.set_curve_id(3)
-    flow_controller_1.set_interpolation_kind(CurveInterpolationType.CATMULL)
+    flow_controller_1.set_interpolation_kind(dic.enums.CurveInterpolationType.CATMULL)
     flow_controller_1.set_points(points)
-    flow_controller_1.set_reference_type(ReferenceType.POSITION)
+    flow_controller_1.set_reference_type(dic.enums.ReferenceType.POSITION)
     flow_controller_1.set_reference_id(4)
     print(json.dumps(flow_controller_1.get_controller(), indent=4))
     
-    temperature_controller_1 = temperature_controller()
-    temperature_controller_1.set_algorithm(Temperature_Algorithm_Type.WATER)
+    temperature_controller_1 = TemperatureController()
+    temperature_controller_1.set_algorithm(dic.enums.Temperature_Algorithm_Type.WATER)
     temperature_controller_1.set_curve_id(5)
-    temperature_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    temperature_controller_1.set_interpolation_kind(dic.enums.CurveInterpolationType.LINEAR)
     temperature_controller_1.set_points(points)
-    temperature_controller_1.set_reference_type(ReferenceType.WEIGHT)
+    temperature_controller_1.set_reference_type(dic.enums.ReferenceType.WEIGHT)
     temperature_controller_1.set_reference_id(6)
     print(json.dumps(temperature_controller_1.get_controller(), indent=4))
     
-    speed_controller_1 = speed_controller()
-    speed_controller_1.set_algorithm(Speed_Algorithm_Type.EASE_IN)
+    speed_controller_1 = SpeedController()
+    speed_controller_1.set_algorithm(dic.enums.Speed_Algorithm_Type.EASE_IN)
     speed_controller_1.set_speed(7)
-    speed_controller_1.set_direction(Direction_Type.FORWARD)
+    speed_controller_1.set_direction(dic.enums.Direction_Type.FORWARD)
     print(json.dumps(speed_controller_1.get_controller(), indent=4))
     
-    power_controller_1 = power_controller()
+    power_controller_1 = PowerController()
     power_controller_1.set_curve_id(7)
-    power_controller_1.set_interpolation_kind(CurveInterpolationType.CATMULL)
+    power_controller_1.set_interpolation_kind(dic.enums.CurveInterpolationType.CATMULL)
     power_controller_1.set_points(points)
-    power_controller_1.set_reference_type(ReferenceType.TIME)
+    power_controller_1.set_reference_type(dic.enums.ReferenceType.TIME)
     power_controller_1.set_reference_id(8)
     print(json.dumps(power_controller_1.get_controller(), indent=4))
     
-    weight_controller_1 = weight_controller()
+    weight_controller_1 = WeightController()
     weight_controller_1.set_curve_id(9)
-    weight_controller_1.set_interpolation_kind(CurveInterpolationType.LINEAR)
+    weight_controller_1.set_interpolation_kind(dic.enums.CurveInterpolationType.LINEAR)
     weight_controller_1.set_points(points)
-    weight_controller_1.set_reference_type(ReferenceType.POSITION)
+    weight_controller_1.set_reference_type(dic.enums.ReferenceType.POSITION)
     weight_controller_1.set_reference_id(10)
     print(json.dumps(weight_controller_1.get_controller(), indent=4))
     
-    log_controller_1 = log_controller()
-    log_controller_1.set_message(Message_Type.NO_WATER) 
+    log_controller_1 = LogController()
+    log_controller_1.set_message(dic.enums.Message_Type.NO_WATER) 
     print(json.dumps(log_controller_1.get_controller(), indent=4))
     
-    tare_controller_1 = tare_controller()
+    tare_controller_1 = TareController()
     print(json.dumps(tare_controller_1.get_controller(), indent=4))
     
-    end_profile_1 = end_profile()
+    end_profile_1 = EndProfile()
     print(json.dumps(end_profile_1.get_controller(), indent=4))
+    
+    time_reference_controller_1 = TimeReferenceController()
+    time_reference_controller_1.set_reference_id(11)
+    print(json.dumps(time_reference_controller_1.get_controller(), indent=4))
+    
+    position_reference_controller_1 = PositionReferenceController()
+    position_reference_controller_1.set_reference_id(12)
+    print(json.dumps(position_reference_controller_1.get_controller(), indent=4))
+    
+    weight_reference_controller_1 = WeightReferenceController()
+    weight_reference_controller_1.set_reference_id(13)
+    print(json.dumps(weight_reference_controller_1.get_controller(), indent=4))
     
---
+++ profile_manager/enums.py
@@ -0,0 +1,125 @@
+from enum import Enum
+
+# Enumerations for the different types of controllers for easy access, validation and maintenance
+class ReferenceType(Enum):
+    TIME = "time"
+    POSITION = "position"
+    WEIGHT = "weight"
+    CURVE = "curve"
+    CONTROL = "control"
+
+class Pressure_Algorithm_Type(Enum):
+    PID_V1 = "pid v1"
+    PID_V2 = "pid v2"
+    
+class Temperature_Algorithm_Type(Enum):
+    WATER = "water"
+    CYLINDER = "cylinder"
+    TUBE = "tube"
+    PLUNGER = "plunger"
+    STABLE = "stable"
+    
+class Power_Algorithm_Type(Enum):
+    SPRING = "spring"
+    
+class Flow_Algorithm_Type(Enum):
+    PID_V1 = "pid v1"
+    
+class Weight_Algorithm_Type(Enum):
+    PID_V1 = "pid v1"    
+    
+class Speed_Algorithm_Type(Enum):
+    EASE_IN = "ease-in"
+    FAST = "fast"
+
+class CurveInterpolationType(Enum):
+    LINEAR = "linear"
+    CATMULL = "catmull"
+    
+class Message_Type(Enum):
+    NO_WATER = "no water"
+    REMOVE_CUP = "remove cup"
+    PURGE = "purge"
+    START_CLICK = "start click"
+    PURGE_CLICK = "purge click"
+    
+class Direction_Type(Enum):
+    FORWARD = "forward"
+    BACKWARD = "backward"
+    
+class AlgorithmType(Enum):
+    PRESSURE = "pressure"
+    POWER = "power"
+    TEMPERATURE = "temperature"
+    FLOW = "flow"
+    WEIGHT = "weight"
+    SPEED = "speed"
+    
+class ControllerType(Enum):
+    POWER = "power"
+    FLOW = "flow"
+    PRESSURE = "pressure"
+    WEIGHT = "weight"
+    SPEED = "speed"
+    TEMPERATURE = "temperature"
+    TARE = "tare"
+    MESSAGE = "message"
+    END = "end"
+    
+#! Triggers type dictionaries.
+
+class TriggerType(Enum):
+    PISTON_POSITION = "piston_position_trigger"
+    SPEED = "speed_trigger"
+    TIME = "time_trigger"
+    WEIGHT = "weight_trigger"
+    BUTTON = "button_trigger"
+    WATER_DETECTION = "water_detection_trigger"
+    EXIT = "exit"
+    VALUE = "value"
+    CURVE = "curve"
+    FLOW = "flow"
+    PRESSURE = "pressure"
+    TEMPERATURE = "temperature"
+    POWER = "power"
+    
+class SourceType(Enum):
+    FLOW = "flow"
+    PRESSURE = "pressure"
+    WEIGHT = "weight"
+    POWER = "power"
+    RAW = "raw"
+    AVERAGE = "average"
+    PREDICTIVE = "predictive"
+    TEMPERATURE = "temperature"
+    BUTTON = "button"
+    GESTURE = "button_gesture"
+    
+class ButtonSourceType(Enum):
+    START = "start"
+    TARE = "tare"
+    ENCODER = "encoder"
+    ENCODER_BUTTON = "encoder button"
+
+class ButtonGestureSourceType(Enum):
+    SINGLE = "single"
+    DOUBLE = "double"
+    RIGHT = "right"
+    LEFT = "left"
+    PRESSED = "pressed"
+    RELEASED = "released"
+    LONG = "long"
+    
+class TemperatureSourceType(Enum):
+    TUBE = "tube"
+    CYLINDER = "cylinder"
+    PLUNGER = "plunger"
+    WATER = "water"
+    CYLINDER_AVERAGE = "cylinder average"
+    
+class TriggerOperatorType(Enum):
+    GREATER_THAN = "greater_than"
+    LESS_THAN = "less_than"
+    EQUAL = "equal"
+    GREATER_THAN_OR_EQUAL = "greater_than_or_equal"
+    LESS_THAN_OR_EQUAL = "less_than_or_equal"
