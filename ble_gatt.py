a3f72948d3e5aa7dea46426e6cca9a2ead5501ea
Mimoja <git@mimoja.de>
2024-02-16 14:12:21 -0600

    Revert "api: Add WIFI rest endpoint"
    This reverts commit 17edc190f172f76d38b9e9d61d7e13a0b0265c35.

--- ble_gatt.py
+++ ble_gatt.py
@@ -97,10 +97,6 @@ class GATTServer():
         if server.state == ImprovState.AWAITING_AUTHORIZATION:
             server.state = ImprovState.AUTHORIZED
 
-    def is_provisioning():
-        server = GATTServer.getServer().improv_server
-        return server.state.value >= ImprovState.AUTHORIZED.value
-
     def is_running(self):
         if self.loopThread is None:
             return False
