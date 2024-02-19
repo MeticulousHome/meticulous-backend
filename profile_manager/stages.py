8615d0b5f8b55de9928ddcebc032e941cac7b9bf
AUOR17 <angel_osorno@outlook.com>
2024-02-19 14:13:33 -0600

    We can introduce values in the class constructor
    This  applies to the following classes:
    
    - nodes.py
    - stages.py
    - triggers.py
    - controllers.py
    
    Also there is an example for how to use the method of these classes.

--- profile_manager/stages.py
+++ profile_manager/stages.py
@@ -19,39 +19,22 @@ class Stages:
 if __name__ == "__main__":
     stage_1 = Stages("Stage 1")
     
-    node = Nodes()
-    node.set_id(1)
+    node = Nodes(1)
     points = [[0, 6],[10,8]]
     
-    controller = FlowController()
-    controller.set_curve_id(1)
-    controller.set_interpolation_kind(CurveInterpolationType.LINEAR)
-    controller.set_points(points)
-    controller.set_reference_type(ReferenceType.TIME)
-    controller.set_reference_id(1)
+    controller = FlowController(Flow_Algorithm_Type.PID_V1, 1, CurveInterpolationType.LINEAR, points, ReferenceType.TIME, 1)
     node.add_controller(controller)
     
-    controller = TimeReferenceController()
-    controller.set_reference_id(2)
+    controller = TimeReferenceController(2)
     node.add_controller(controller)
 
-    trigger = SpeedTrigger()
-    trigger.set_value(1)
-    trigger.set_operator(TriggerOperatorType.GREATER_THAN_OR_EQUAL)
-    trigger.set_next_node_id(1)
+    trigger = SpeedTrigger(TriggerOperatorType.GREATER_THAN_OR_EQUAL, 10, 2)
     node.add_trigger(trigger)
     
-    node_1 = Nodes()
-    node_1.set_id(2)
+    node_1 = Nodes(2)
     points = [[10, 15],[20,18]]
     
-    controller = PressureController()
-    controller.set_algorithm(Pressure_Algorithm_Type.PID_V1)
-    controller.set_curve_id(2)
-    controller.set_interpolation_kind(CurveInterpolationType.LINEAR)
-    controller.set_points(points)
-    controller.set_reference_type(ReferenceType.POSITION)
-    controller.set_reference_id(2)
+    controller = PressureController(Pressure_Algorithm_Type.PID_V1, 4, CurveInterpolationType.CATMULL, points, ReferenceType.POSITION, 2)
     node_1.add_controller(controller)
     
     stage_1.add_node(node)
@@ -60,25 +43,16 @@ if __name__ == "__main__":
     stage_2 = Stages()
     stage_2.set_name("Stage 2")
     
-    node_2 = Nodes()
-    node_2.set_id(3)
+    node_2 = Nodes(3)
     points = [[20, 25],[30,28]]
     
-    controller = PressureController()
-    controller.set_algorithm(Pressure_Algorithm_Type.PID_V1)
-    controller.set_curve_id(3)
-    controller.set_interpolation_kind(CurveInterpolationType.LINEAR)
-    controller.set_points(points)
-    controller.set_reference_type(ReferenceType.POSITION)
-    controller.set_reference_id(3)
+    controller = PressureController(Pressure_Algorithm_Type.PID_V1, 5, CurveInterpolationType.CATMULL, points, ReferenceType.POSITION, 3)
     node_2.add_controller(controller)
     
     controller = TareController()
     node_2.add_controller(controller)
     
-    trigger = SpeedTrigger()
-    trigger.set_value(2)
-    trigger.set_next_node_id(3)
+    trigger = SpeedTrigger(TriggerOperatorType.GREATER_THAN_OR_EQUAL, 20, 3)
     node_2.add_trigger(trigger)
     
     node_3 = Nodes()
@@ -89,23 +63,13 @@ if __name__ == "__main__":
     controller.set_reference_id(4)
     node_3.add_controller(controller)
     
-    controller = WeightController()
-    controller.set_curve_id(4)
-    controller.set_interpolation_kind(CurveInterpolationType.CATMULL)
-    controller.set_points(points)
-    controller.set_reference_type(ReferenceType.WEIGHT)
-    controller.set_reference_id(4)
+    controller = WeightController(Weight_Algorithm_Type.PID_V1, 200, CurveInterpolationType.LINEAR, points, ReferenceType.WEIGHT, 4)
     node_3.add_controller(controller)
     
-    trigger = WeightTrigger()
-    trigger.set_value(3)
-    trigger.set_weight_reference_id(200)
-    trigger.set_next_node_id(4)
+    trigger = WeightTrigger(SourceType.AVERAGE, TriggerOperatorType.GREATER_THAN_OR_EQUAL,3, 200, 4)
     node_3.add_trigger(trigger)
     
-    trigger = ExitTrigger()
-    # trigger.set_value(4)
-    trigger.set_next_node_id(1)
+    trigger = ExitTrigger(1)
     node_3.add_trigger(trigger)
     
     stage_2.add_node(node_2)
