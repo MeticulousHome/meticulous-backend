7d5dc111c76c4ba560b2213aade3c00efe6393b5
Mimoja <mimoja@meticuloushome.com>
2024-02-26 12:55:04 -0600

    api/actions: explicitly check that only certain actions are allowed to protect the ESP
    The user really only needs start, stop, reset and tare and we are therefore limiting
    the api to those, also to not allow for potential command injections (those are still very
    much possible via socket.io)
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- api/action.py
+++ api/action.py
@@ -8,10 +8,20 @@ logger = MeticulousLogger.getLogger(__name__)
 
 class ExecuteActionHandler(BaseHandler):
     def get(self, action):
-        Machine.action(action_event=action)
-        self.write(json.dumps(
-            {"action": action, "status": "send"}
-        ))
+        allowed_actions = ["start, stop, reset, tare"]
+        if (action in allowed_actions):
+            Machine.action(action_event=action)
+            self.write(json.dumps(
+                {"action": action, "status": "ok"}
+            ))
+        else:
+            self.set_status(400)
+            self.write(json.dumps(
+                {"status": "error",
+                 "action": action,
+                 "allowed_actions": allowed_actions}
+            ))
+
 
 ACTIONS_HANDLER = [
     (r"/action/(.*)", ExecuteActionHandler),
