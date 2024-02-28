27b734c82d03f784dbf6a6fa3db9baa4dd226046
Mimoja <mimoja@meticuloushome.com>
2024-02-27 19:47:37 -0600

    Fix profiles and settings endpoints
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- api/settings.py
+++ api/settings.py
@@ -1,15 +1,17 @@
-import tornado.ioloop
-import tornado.web
 import json
 
-from config import MeticulousConfig, CONFIG_USER
+import tornado.ioloop
+import tornado.web
+from log import MeticulousLogger
+
+from config import CONFIG_USER, MeticulousConfig
 
 from .base_handler import BaseHandler
-from log import MeticulousLogger
 
 logger = MeticulousLogger.getLogger(__name__)
 
-class GetSettingsHandler(BaseHandler):
+
+class SettingsHandler(BaseHandler):
     def get(self, setting_name=None):
         if setting_name:
             setting = MeticulousConfig[CONFIG_USER].get(setting_name)
@@ -20,36 +22,40 @@ class GetSettingsHandler(BaseHandler):
                 self.write(json.dumps(response))
             else:
                 self.set_status(404)
-                self.write(f"Setting {setting_name} not found!")
+                self.write(
+                    {"status": "error", "error": "setting not found", "setting": setting_name})
         else:
             self.write(json.dumps(MeticulousConfig[CONFIG_USER]))
 
-class UpdateSettingsHandler(BaseHandler):
     def post(self):
         try:
             settings = json.loads(self.request.body)
         except json.decoder.JSONDecodeError as e:
             self.set_status(403)
-            self.write(f"Settings change failed: {e}")
+            self.write({"status": "error", "error": "invalid json", "json_error": f"{e}"})
             return
 
         for setting_name in settings:
             value = settings.get(setting_name)
+            if value not in [True, False]:
+                self.set_status(404)
+                self.write({"status": "error", "error": "setting value invalidm, expected boolean",
+                           "setting": setting_name, "value": value})
+                MeticulousConfig.load()
+                return
             setting = MeticulousConfig[CONFIG_USER].get(setting_name)
             if setting != None:
                 MeticulousConfig[CONFIG_USER][setting_name] = value
-                response = {
-                    setting_name: MeticulousConfig[CONFIG_USER][setting_name]
-                }
-                self.write(json.dumps(response))
             else:
                 self.set_status(404)
-                self.write(f"Setting '{setting_name}' failed: Setting not found!")
+                self.write(
+                    {"status": "error", "error": "setting not found", "setting": setting_name})
                 MeticulousConfig.load()
                 return
         MeticulousConfig.save()
+        return self.get()
+
 
 SETTINGS_HANDLER = [
-        (r"/settings/*", UpdateSettingsHandler),
-        (r"/settings/?(.*)", GetSettingsHandler),
-    ]
+    (r"/settings/*", SettingsHandler),
+]
