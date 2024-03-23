40931457e702288109e4fbaadf4d367f216050aa
AUOR17 <angel_osorno@outlook.com>
2024-03-23 12:53:33 -0600

    feat(profile_converter): Add peaks analyzer.
    - Add a node to analyze if the temperature is in range according to a temperature offset

--- profile_converter/profile_converter.py
+++ profile_converter/profile_converter.py
@@ -245,6 +245,7 @@ class ComplexProfileConverter:
                     {
                         "kind": "temperature_value_trigger",
                         "next_node_id": 7,
+                        "next_node_id": 7,
                         "source": "Water Temperature",
                         "operator": ">=",
                         "value": self.temperature - self.offset_temperature
