2a882a4acf398b0714a27c1fd1c72028d15788bc
Octavio2121 <tavo._22@hotmail.com>
2024-05-29 17:23:06 -0600

    Remove dbus messages

--- api/update_os.py
+++ api/update_os.py
@@ -12,13 +12,8 @@ class StartOSUpdateHandler(BaseHandler):
         RaucManager.start_os_update()  # Run the command in a separate thread
         self.write("Update process started. Check show_update_messages for progress.")
 
-class ShowUpdateMessagesHandler(BaseHandler):
-    def get(self):
-        update_info = RaucManager.get_update_messages()
-        self.write(update_info.output)
-
 # Register the routes when importing the module
 API.register_handler(APIVersion.V1, r"/update_os/rauc_status", RAUCStatusHandler)
 API.register_handler(APIVersion.V1, r"/update_os/start_os_update", StartOSUpdateHandler)
-API.register_handler(APIVersion.V1, r"/update_os/show_update_messages", ShowUpdateMessagesHandler)
+
 
