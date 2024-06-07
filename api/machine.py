74d968286f62d391d3f76ff8bfcceb97b3660488
Mimoja <mimoja@meticuloushome.com>
2024-06-07 22:23:03 +0200

    machine: save serial and color in settings for easier retrieval. Mock data for now
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- api/machine.py
+++ api/machine.py
@@ -8,7 +8,7 @@ from wifi import WifiManager
 
 from .api import API, APIVersion
 from .base_handler import BaseHandler
-
+from config import MeticulousConfig, CONFIG_SYSTEM, MACHINE_COLOR, MACHINE_SERIAL_NUMBER
 logger = MeticulousLogger.getLogger(__name__)
 
 
@@ -18,15 +18,18 @@ class MachineInfoHandler(BaseHandler):
         config = WifiManager.getCurrentConfig()
         response["name"] = HostnameManager.generateDeviceName()
         response["hostname"] = config.hostname
+        response["color"] = ""
+        response["serial"] = ""
+        response["model_version"] = "v10.1.0"
 
         if Machine.esp_info is not None:
             response["firmware"] = Machine.esp_info.firmwareV
 
-        response["color"] = "black"
-        response["model_version"] = "v10.1.0"
-        response["serial"] = "103"
-
-        response["mock_response"] = True
+        if MeticulousConfig[CONFIG_SYSTEM][MACHINE_SERIAL_NUMBER] is not None:
+            response["serial"] = MeticulousConfig[CONFIG_SYSTEM][MACHINE_SERIAL_NUMBER]
+            
+        if MeticulousConfig[CONFIG_SYSTEM][MACHINE_SERIAL_NUMBER] is not None:
+            response["color"] = MeticulousConfig[CONFIG_SYSTEM][MACHINE_COLOR]
 
         self.write(json.dumps(response))
 
