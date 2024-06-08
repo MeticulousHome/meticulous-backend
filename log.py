1086b8c398cb39089d6df93ea0285c53b40defcc
Mimoja <mimoja@meticuloushome.com>
2024-06-08 22:17:44 +0200

    logs: color logs and change on-disk log format
    Over the months the startup logging has grown quiet substantially and is significantly
    more difficult to read.
    We are therefore adding colors to the logs as well as timestamps.
    This should help debugging and also looks generally appealing :)
    
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- log.py
+++ log.py
@@ -3,18 +3,27 @@ import logging
 import logging.handlers
 import os
 import shutil
+import coloredlogs
+
+LOGLEVEL = os.getenv("LOGLEVEL", "DEBUG").upper()
+LOG_PATH = os.getenv("LOG_PATH", "/meticulous-user/logs")
+
+logformat = "%(asctime)s %(name)s %(levelname)s %(message)s"
+coloredlogs.install(fmt=logformat, level=LOGLEVEL, milliseconds=True)
 
 logging.basicConfig()
 
-LOG_PATH = os.getenv("LOG_PATH", '/meticulous-user/logs')
 
 class MeticulousLogger:
     _rh = None
     _f = None
     _sh = None
 
-    LOGLEVEL=os.getenv('LOGLEVEL', 'DEBUG').upper()
-    FORCE_STDOUT_LOG=os.getenv("FORCE_STDOUT_LOG", 'False').lower() in ('true', '1', 'y')
+    FORCE_STDOUT_LOG = os.getenv("FORCE_STDOUT_LOG", "False").lower() in (
+        "true",
+        "1",
+        "y",
+    )
 
     def setFileLogLevel(level):
         if MeticulousLogger._rh is None:
@@ -27,8 +36,8 @@ class MeticulousLogger:
 
     # function called to rotatethe log at a certain size or time
     def cb_logrotate(source, dest):
-        with open(source, 'rb') as f_in:
-            with gzip.open(dest, 'wb') as f_out:
+        with open(source, "rb") as f_in:
+            with gzip.open(dest, "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
         os.remove(source)
 
@@ -36,14 +45,15 @@ class MeticulousLogger:
         MB = 1024 * 1024
         # Create directory for the logfiles if it doesn't exist
         os.makedirs(LOG_PATH, exist_ok=True)
-        logfilePath = os.path.join(LOG_PATH, 'backend.log')
-        
+        logfilePath = os.path.join(LOG_PATH, "backend.log")
+
         MeticulousLogger._rh = logging.handlers.RotatingFileHandler(
-            logfilePath, maxBytes=200*MB, backupCount=10)
+            logfilePath, maxBytes=200 * MB, backupCount=10
+        )
         MeticulousLogger._rh.rotator = MeticulousLogger.cb_logrotate
         MeticulousLogger._rh.namer = MeticulousLogger.cb_logname
 
-        MeticulousLogger._f = logging.Formatter(logging.BASIC_FORMAT)
+        MeticulousLogger._f = logging.Formatter(logformat)
         MeticulousLogger._rh.setFormatter(MeticulousLogger._f)
         MeticulousLogger._rh.doRollover()
         MeticulousLogger._sh = logging.StreamHandler()
@@ -56,7 +66,10 @@ class MeticulousLogger:
         if MeticulousLogger._rh not in logger.handlers:
             logger.addHandler(MeticulousLogger._rh)
         # This will lead to double logging on some systems, so it is disabled by default
-        if MeticulousLogger.FORCE_STDOUT_LOG and MeticulousLogger._sh not in logger.handlers:
+        if (
+            MeticulousLogger.FORCE_STDOUT_LOG
+            and MeticulousLogger._sh not in logger.handlers
+        ):
             logger.addHandler(MeticulousLogger._sh)
-        logger.setLevel(MeticulousLogger.LOGLEVEL)
+        logger.setLevel(LOGLEVEL)
         return logger
