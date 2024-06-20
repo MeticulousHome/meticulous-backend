5961e8d0fc451bf2aa20edf5d376b55dce719870
Mimoja <mimoja@meticuloushome.com>
2024-06-20 14:00:16 +0200

    api: fix shot history api endpoint
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- api/history.py
+++ api/history.py
@@ -18,37 +18,53 @@ last_version_path = f"/api/{APIVersion.latest_version().name.lower()}"
 
 
 class ZstdHistoryHandler(tornado.web.StaticFileHandler):
-    async def get(self, path, include_body=True):
+    async def get(self, path):
+        self.set_header("Content-Type", "application/json")
+
         # Check if the path is a directory
         full_path = self.get_absolute_path(self.root, path)
+        # Needed for caching in the tornado.web.StaticFileHandler class
+        self.absolute_path = full_path
+
+        compressed_path = f"{full_path}.zst"
         if os.path.isdir(full_path):
             # If it's a directory, show the JSON listing
-            return await self.list_directory(path)
+            return await self.list_directory(full_path)
         elif not os.path.exists(full_path) or not os.path.isfile(full_path):
-            logger.info(f"File doesn't exist: {full_path}")
-            # If the path doesn't exist or isn't a file, raise a 404 error
-            raise tornado.web.HTTPError(404)
-        elif full_path.endswith(".zst"):
-            self.absolute_path = full_path
-            # Handle .zstd compressed file
-            return await self.serve_zstd_file(full_path, include_body)
-        else:
-            # Fallback to default behavior for regular files
-            return await super().get(path, include_body)
+            logger.info(f"File doesn't exist: {full_path}, checking if it exists compressed")
 
-    async def serve_zstd_file(self, full_path, include_body):
-        logger.info(f"Serving File: {include_body}")
+            if os.path.exists(compressed_path) and os.path.isfile(compressed_path):
+                logger.info("File exists compressed instead")
+                self.absolute_path = compressed_path
+                return await self.serve_zstd_file(compressed_path)
+
+            # If the path doesn't exist or isn't a file, raise a 404 error
+            self.set_status(404)
+            self.write(
+                {"status": "error", "error": "history entry not found", "path": path})
+            return
+        elif full_path.endswith(".zst"):
+            logger.info("dealing")
+            # Handle .zstd compressed file
+            return await self.serve_zstd_file(full_path)
+        else:
+            logger.info("File exists on disk")
+            # Fallback to default behavior for regular files
+            response = await super().get(path, True)
+            self.set_header("Content-Type", "application/json")
+            return response
+
+    async def serve_zstd_file(self, full_path):
+        logger.info(f"Serving File: {full_path}")
         with open(full_path, 'rb') as compressed_file:
             decompressor = zstd.ZstdDecompressor()
             decompressed_content = decompressor.stream_reader(compressed_file)
-            if include_body:
-                self.set_header("Content-Type", self.get_content_type())
-                self.write(decompressed_content.read())
+            self.set_header("Content-Type", "application/json")
+            self.write(decompressed_content.read())
             self.finish()
 
-    async def list_directory(self, path):
-        full_path = self.get_absolute_path(self.root, path)
-        self.absolute_path = full_path
+    async def list_directory(self, full_path):
+
         if not os.path.isdir(full_path):
             raise tornado.web.HTTPError(404)
 
@@ -63,10 +79,6 @@ class ZstdHistoryHandler(tornado.web.StaticFileHandler):
         self.write(json.dumps(files_info))
         self.finish()
 
-    def get_content_type(self):
-        # Ensure content type for served files is correct
-        return 'application/octet-stream'
-
 
 API.register_handler(APIVersion.V1, r'/history/debug',
                      tornado.web.RedirectHandler, url=f"{last_version_path}/history/debug/"),
@@ -77,5 +89,5 @@ API.register_handler(APIVersion.V1, r'/history/debug/(.*)',
 API.register_handler(APIVersion.V1, r'/history',
                      tornado.web.RedirectHandler, url=f"{last_version_path}/history/"),
 
-API.register_handler(APIVersion.V1, r'/history/(((?!debug)).*)',
+API.register_handler(APIVersion.V1, r'/history/(.*)',
                      ZstdHistoryHandler, path=HISTORY_PATH),
