8aaefd13e28acaae82397541775748b54f564e04
Mimoja <mimoja@meticuloushome.com>
2024-07-20 19:08:51 +0200

    Use sentry for crash tracking
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- back.py
+++ back.py
@@ -1,3 +1,27 @@
+import sentry_sdk
+from sentry_sdk.integrations.asyncio import AsyncioIntegration
+
+import os
+BACKEND = os.getenv("BACKEND", "FIKA").upper()
+
+if BACKEND == "FIKA":
+    print("Initializing sentry")
+    sentry_sdk.init(
+        dsn="https://0b7872daf08aae52a8d654472bc8bb26@o4506723336060928.ingest.us.sentry.io/4507635208224768",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=0.0,
+        integrations=[
+            AsyncioIntegration(),
+        ],
+       )
+else:
+    print("Skipping Sentry initialization")
+
 from tornado.options import define, options, parse_command_line
 import socketio
 import tornado.log
