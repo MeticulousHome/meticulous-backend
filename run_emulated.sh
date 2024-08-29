7b25c332c95eaa7c411b40bfb6e4a803f36fb808
Mimoja <mimoja@meticuloushome.com>
2024-08-29 18:39:00 -0400

    Add memray option for local running
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- run_emulated.sh
+++ run_emulated.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 export BACKEND=emulation
 export CONFIG_PATH=./config
 export LOG_PATH=./logs
@@ -12,4 +12,15 @@ export USER_SOUNDS=./sounds
 export DEFAULT_IMAGES=./images/default
 export IMAGES_PATH=./images/profile-images
 export DEFAULT_PROFILES=./default_profiles
-python3 back.py
+
+if [[ "$@" == *"--memory"* ]]; then
+    if [ python3 -m memray ]; then
+        python3 -m pip install memray
+    fi
+    profiling_file="memory_profiling_$(date -Iseconds).bin"
+    python3 -m memray run -o ${profiling_file} back.py
+    python3 -m memray flamegraph ${profiling_file}
+    python3 -m memray summary ${profiling_file}
+else
+    python3 back.py
+fi
