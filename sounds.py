4c6b07b0d03b58c30ed3cf86ee6ded8ccc0ae6de
Mimoja <mimoja@meticuloushome.com>
2024-08-30 21:57:17 -0400

    Fully disable all calls to playsound while we debug an ALSA issue
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- sounds.py
+++ sounds.py
@@ -167,7 +167,10 @@ class SoundPlayer:
         logger.info(f"Playing {sound_name} from {file_path}")
 
         try:
-            playsound(file_path, block=False)
+            logger.warning(
+                f"No playing sound {file_path}. All sounds are disable to debug memory leaks!"
+            )
+            # playsound(file_path, block=False)
         except Exception as e:
             logger.exception(f"Failed to play sound: {e}")
             return False
