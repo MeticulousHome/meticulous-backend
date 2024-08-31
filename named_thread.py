b31f9154a232c7706c4992e22b3bc0f2b1de7466
Mimoja <mimoja@meticuloushome.com>
2024-08-30 20:30:04 -0400

    NamedThread: add missing file
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

---
+++ named_thread.py
@@ -0,0 +1,21 @@
+import pyprctl
+import threading
+
+from log import MeticulousLogger
+
+logger = MeticulousLogger.getLogger(__name__)
+
+
+class NamedThread(threading.Thread):
+    def __init__(self, name: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name: str = name
+
+    def run(self):
+        try:
+            pyprctl.set_name(self.name)
+            logger.warning(f"Thread {self.name} started")
+            super().run()
+        except Exception:
+            # FIXME we should handle crashes here
+            pass
