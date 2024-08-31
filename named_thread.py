72380088b61e87098a1675bd4603617dfce898bd
Mimoja <mimoja@meticuloushome.com>
2024-08-30 21:57:00 -0400

    NamedThread: reduce logging level
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- named_thread.py
+++ named_thread.py
@@ -14,7 +14,7 @@ class NamedThread(threading.Thread):
     def run(self):
         try:
             pyprctl.set_name(self.name)
-            logger.warning(f"Thread {self.name} started")
+            logger.debug(f"Thread {self.name} started")
             super().run()
         except Exception:
             # FIXME we should handle crashes here
