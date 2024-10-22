3cba360c776e886477a3b03a3ac534e7e81ad098
Mimoja <mimoja@meticuloushome.com>
2024-10-22 20:59:49 +0200

    backlight: move thread logic into the backlight controller
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- backlight_controller.py
+++ backlight_controller.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 from log import MeticulousLogger
 
@@ -10,29 +11,18 @@ MIN_BRIGHTNESS = 512
 
 
 class BacklightController:
+    _adjust_thread = None
 
-    def get_current_brightness():
-        with open(BRIGHTNESS_FILE, "r") as f:
-            return int(f.read().strip())
+    def stop_adjust_thread():
+        if (
+            BacklightController._adjust_thread
+            and BacklightController._adjust_thread.is_alive()
+        ):
+            BacklightController._adjust_thread.do_run = False
+            BacklightController._adjust_thread.join()
 
-    def set_brightness(value):
-        with open(BRIGHTNESS_FILE, "w") as f:
-            f.write(str(value))
-
-    # Linear interpolation
-    def linear_interpolation(start, end, steps):
-        step_size = (end - start) / steps
-        for i in range(steps):
-            yield start + step_size * i
-
-    # Quadratic interpolation (curve)
-    def curve_interpolation(start, end, steps):
-        for i in range(steps):
-            t = i / steps
-            yield start + (end - start) * (t**2)
-
-    # Soft dimming function
-    def adjust_brightness(target_brightness, interpolation="linear", steps=50):
+    def adjust_brightness_thread(target_brightness, interpolation="linear", steps=50):
+        t = threading.currentThread()
         current_brightness = BacklightController.get_current_brightness()
 
         if interpolation == "linear":
@@ -46,14 +36,22 @@ class BacklightController:
         else:
             raise ValueError("Interpolation must be 'linear' or 'curve'")
 
-        # Softly adjust the brightness
         for brightness in interpolator:
+            if getattr(t, "do_run", True) is False:
+                break
             BacklightController.set_brightness(int(brightness))
             time.sleep(0.01)
 
+    def adjust_brightness(target_brightness, interpolation="linear", steps=50):
+        BacklightController.stop_adjust_thread()
+        BacklightController._adjust_thread = threading.Thread(
+            target=BacklightController.adjust_brightness_thread,
+            args=(target_brightness, interpolation, steps),
+        )
+        BacklightController._adjust_thread.start()
+
     def dim_down():
         target_brightness = MIN_BRIGHTNESS
-
         try:
             BacklightController.adjust_brightness(
                 target_brightness, interpolation="curve", steps=300
@@ -63,7 +61,6 @@ class BacklightController:
 
     def dim_up():
         target_brightness = MAX_BRIGHTNESS
-
         try:
             BacklightController.adjust_brightness(
                 target_brightness, interpolation="linear", steps=20
