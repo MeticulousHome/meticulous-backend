aea9a1fb1964fc15598b7b810bc04a884cafbff6
Angel-Padilla <angel.p.lopez@outlook.com>
2024-12-04 19:45:24 -0600

    fix:  endpoint capture clause added to get the setting name to update

--- api/settings.py
+++ api/settings.py
@@ -185,5 +185,5 @@ class TimezoneUIProvider(BaseHandler):
         )
 
 
-API.register_handler(APIVersion.V1, r"/settings/*", SettingsHandler),
+API.register_handler(APIVersion.V1, r"/settings/(.*)", SettingsHandler),
 API.register_handler(APIVersion.V1, r"/timezones/(.*)", TimezoneUIProvider),
