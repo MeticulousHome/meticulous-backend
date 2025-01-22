acebb9628afef618bec8c83817e44219d7f90df8
Mimoja <mimoja@mimoja.de>
2025-01-22 12:26:29 +0100

    ota: Add BUILD_DATE parsing to the UpdateManager
    Signed-off-by: Mimoja <mimoja@mimoja.de>

--- ota.py
+++ ota.py
@@ -1,6 +1,7 @@
 import subprocess
+import os
 from pathlib import Path
-
+from datetime import datetime
 from log import MeticulousLogger
 
 logger = MeticulousLogger.getLogger(__name__)
@@ -10,6 +11,9 @@ HAWKBIT_CHANNEL_FILE = "channel"
 
 
 class UpdateManager:
+
+    ROOTFS_BUILD_DATE = None
+    BUILD_DATE_FILE = "/opt/ROOTFS_BUILD_DATE"
 
     @staticmethod
     def setChannel(channel: str):
@@ -35,3 +39,40 @@ class UpdateManager:
                 subprocess.run(["systemctl", "restart", "rauc-hawkbit-updater"])
             except Exception as e:
                 logger.error(f"Failed to change update channel: {e}")
+
+    @staticmethod
+    def getBuildTimestamp():
+        if UpdateManager.ROOTFS_BUILD_DATE is not None:
+            return UpdateManager.ROOTFS_BUILD_DATE
+
+        if not os.path.exists(UpdateManager.BUILD_DATE_FILE):
+            logger.info("BUILD_DATE file not found")
+            return None
+
+        with open(UpdateManager.BUILD_DATE_FILE, "r") as file:
+            date_string = file.read().strip()
+
+        try:
+            build_time = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S %z")
+            return build_time
+        except ValueError:
+            logger.error("Invalid date format in BUILD_DATE file")
+            return None
+
+    @staticmethod
+    def forward_time():
+        target_time = UpdateManager.getBuildTimestamp()
+        if target_time is None:
+            logger.error("Could not get build timestamp")
+            return
+
+        current_time = datetime.now(target_time.tzinfo)
+
+        # Forward time only if it is older than the image itself
+        if current_time < target_time:
+            formatted_time = target_time.strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                subprocess.run(["date", "-s", formatted_time], check=True)
+                print(f"System time updated to: {formatted_time}")
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to set system time: {e}")
