09cba45fc8d60d6fc8ce5a3ff670834d9172172e
Angel-Padilla <angel.p.lopez@outlook.com>
2025-02-04 02:08:57 -0600

    fix: multi update settings via SettingsHandler
    When usind the SettingsHandler, the workflow should allow to modify
    multiple settings with one request, but rn if any of the provided
    settings fail to update correctly, any further setting changes are
    aborted as the function returns.
    
    Now, the function can handle individual failure cases for the already
    defined settings. All the errors encountered are reported back in the
    request response body and the status code for the request can vary from
    `200 (OK)` when no settings encounter errors, `207 (Multi-status)` when at
    least 1 setting was successfully updated and `404 (not found)` when none
    of the settings were successfully updated
    
    In order to avoid the method USBManager.setUSBMode() to rely on the
    loaded value of the MeticulousConfig dictionary -that made necessary to
    update it first before calling it- the value is passed as a parameter to
    the function, also in case that the value is not valid, a RuntimeError is
    raised

--- usb.py
+++ usb.py
@@ -16,18 +16,26 @@ class USBManager:
             return
         USBManager.usbPdController = PTN5150H()
         logger.info("USB PD Controller initialized")
-        USBManager.setUSBMode()
+        try:
+            USB_MODES(MeticulousConfig[CONFIG_USER][USB_MODE])
+            USBManager.setUSBMode(MeticulousConfig[CONFIG_USER][USB_MODE])
+        except RuntimeError as error:
+            logger.error(
+                f"error initializing USB Manager: f{error}, starting USB as client"
+            )
+            USBManager.setUSBMode(USB_MODES.CLIENT.value)
 
     @staticmethod
-    def setUSBMode():
+    def setUSBMode(new_usb_mode):
         logger.warning("Setting USB mode is temporarely disabled")
         return
+
         if Machine.emulated:
             logger.info("Emulated machine, skipping USB mode setup")
             return
         ptn = USBManager.usbPdController
 
-        match MeticulousConfig[CONFIG_USER][USB_MODE]:
+        match new_usb_mode:
             case USB_MODES.CLIENT.value:
                 logger.info("Setting USB mode to CLIENT")
                 ptn.set_port_state(PortState.UFP)
@@ -37,9 +45,7 @@ class USBManager:
             case USB_MODES.DUAL.value:
                 logger.info("Setting USB mode to DUAL")
                 ptn.set_port_state(PortState.DRP)
-
             case _:
-                logger.error(
-                    f"Unknown USB mode {MeticulousConfig[CONFIG_USER][USB_MODE]}"
-                )
+                logger.error(f"Unknown USB mode {new_usb_mode}")
+                raise RuntimeError(f"Unknown USB mode {new_usb_mode}")
         ptn.set_rp_selection(RpSelection.X330_uA)
