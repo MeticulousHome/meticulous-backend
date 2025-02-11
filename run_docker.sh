f4f8825d9cf86310a62d40c6541f7c8029a6b117
Angel-Padilla <angel.p.lopez@outlook.com>
2025-02-10 21:07:01 -0600

    chore: force use of Dockerfile CMD command

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose run --build -p 8080:8080 backend /opt/venv/bin/python3 /app/back.py
+USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose run --build -p 8080:8080 backend
