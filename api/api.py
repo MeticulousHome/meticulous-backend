452c387a76eabe642e3cf56179a9f51818df115b
oscarlr16 <oscarleon.ro16@gmail.com>
2025-02-28 20:19:35 +0000

    shot_rating: remove shot_rating.py

--- api/api.py
+++ api/api.py
@@ -49,7 +49,6 @@ class API:
         from . import sounds as _sounds  # noqa
         from . import machine as _machine  # noqa
         from . import serial as _serial  # noqa
-        from . import shot_rating as _shot_rating  # noqa
 
         routes = []
         logger.info("API Routes registered:")
