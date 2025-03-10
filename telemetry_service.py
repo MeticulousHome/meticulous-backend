93228d89e708f725c333028a545c711b6c12ece6
Mimoja <git@mimoja.de>
2025-03-10 18:28:30 +0100

    telemetry: use meticulous file upload service
    Signed-off-by: Mimoja <git@mimoja.de>

--- telemetry_service.py
+++ telemetry_service.py
@@ -1,8 +1,18 @@
-from config import MeticulousConfig, CONFIG_USER, MACHINE_DEBUG_SENDING
-from notifications import NotificationManager, Notification, NotificationResponse
 from datetime import datetime
-from log import MeticulousLogger
+import json
+import aiohttp
 from sentry_sdk import capture_message
+
+from config import (
+    CONFIG_SYSTEM,
+    CONFIG_USER,
+    MACHINE_DEBUG_SENDING,
+    MACHINE_SERIAL_NUMBER,
+    MeticulousConfig,
+)
+from log import MeticulousLogger
+from notifications import Notification, NotificationManager, NotificationResponse
+from hostname import HostnameManager
 
 logger = MeticulousLogger.getLogger(__name__)
 
@@ -46,3 +56,23 @@ class TelemetryService:
             callback=TelemetryService.onNotificationCallback,
         )
         NotificationManager.add_notification(TelemetryService.permissionNotification)
+
+    # Upload a debug shot file to the server
+    async def upload_debug_shot(file_bytes: bytes):
+        machine_name = f"{HostnameManager.generateHostname()}-{MeticulousConfig[CONFIG_SYSTEM][MACHINE_SERIAL_NUMBER]}"
+        url = f"https://analytics.meticulousespresso.com/upload/{machine_name}"
+        logger.info(f"Uploading debug shot to {url}")
+        data = aiohttp.FormData()
+        data.add_field("file", file_bytes, filename="filename")
+        config = {"config": MeticulousConfig}
+        data.add_field("json", json.dumps(config), content_type="application/json")
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, data=data) as response:
+                    response.raise_for_status()
+        except aiohttp.ClientError as e:
+            logger.info(f"Upload failed: {e}")
+            raise e
+
+        logger.info("Sent debug shot to server")
