6cef939305af7b91b9b0ca691d8bb83241267cd4
Mimoja <git@mimoja.de>
2025-05-05 21:39:35 +0200

    database: add alembic naming conventions
    Signed-off-by: Mimoja <git@mimoja.de>

--- database_models.py
+++ database_models.py
@@ -11,7 +11,15 @@ from sqlalchemy import (
 )
 
 
-metadata = MetaData()
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
 fts_metadata = MetaData()
 
 # Define all tables
