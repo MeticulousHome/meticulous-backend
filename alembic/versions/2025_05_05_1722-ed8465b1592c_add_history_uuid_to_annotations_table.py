fd7cb8a5401738286ccaecc83e550a2b80288069
Mimoja <git@mimoja.de>
2025-05-05 22:16:06 +0200

    alembic: follow the new naming scheme
    Signed-off-by: Mimoja <git@mimoja.de>

--- alembic/versions/2025_05_05_1722-ed8465b1592c_add_history_uuid_to_annotations_table.py
+++ alembic/versions/2025_05_05_1722-ed8465b1592c_add_history_uuid_to_annotations_table.py
@@ -1,31 +1,42 @@
-"""Add history_uuid to annotations table
+"""Add history_uuid to shot annotation
 
-Revision ID: ed8465b1592c
+Revision ID: 1a598cd3ace3
 Revises: 0bdd1c635e7a
-Create Date: 2025-05-05 17:22:10.924676
+Create Date: 2025-05-05 22:00:42.393323
 
 """
 
 from typing import Sequence, Union
 
 from alembic import op
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
-revision: str = "ed8465b1592c"
+revision: str = "1a598cd3ace3"
 down_revision: Union[str, None] = "0bdd1c635e7a"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
-HISTORY_UUID_CONSTRAINT_FOREIGN = "fk_history_uuid"
-
 
 def upgrade() -> None:
+
+    # Get the table constraints up to date with the new naming convention
     with op.batch_alter_table("shot_annotation", schema=None) as batch_op:
-        batch_op.create_foreign_key(
-            HISTORY_UUID_CONSTRAINT_FOREIGN, "history", ["history_uuid"], ["uuid"]
-        )
+        batch_op.create_unique_constraint(None, ["history_id"])
+        batch_op.create_foreign_key(None, "history", ["history_id"], ["id"])
+
+    with op.batch_alter_table("shot_rating", schema=None) as batch_op:
+        batch_op.create_unique_constraint(None, ["annotation_id"])
+
+    # Add history_uuid to shot_annotation
+    with op.batch_alter_table("shot_annotation", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("history_uuid", sa.Text(), nullable=False))
+        batch_op.create_unique_constraint(None, ["history_uuid"])
+        batch_op.create_foreign_key(None, "history", ["history_uuid"], ["uuid"])
 
 
 def downgrade() -> None:
+
     with op.batch_alter_table("shot_annotation", schema=None) as batch_op:
-        batch_op.drop_constraint(HISTORY_UUID_CONSTRAINT_FOREIGN, type_="foreignkey")
+        batch_op.drop_column("history_uuid")
