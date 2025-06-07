56e53a8d437aba4c26a24fefc2ef39777347acf7
Angel-Padilla <angel.p.lopez@outlook.com>
2025-06-06 19:01:34 -0600

    run formatter

--- db_migration_updater.py
+++ db_migration_updater.py
@@ -8,10 +8,6 @@ from log import MeticulousLogger
 from shot_database import DATABASE_URL
 import os
 import shutil
-import importlib
-import importlib.machinery
-import importlib.util
-from types import ModuleType
 
 logger = MeticulousLogger.getLogger(__name__)
 
@@ -36,34 +32,40 @@ ALEMBIC_DIR = os.path.join(BASE_DIR, "alembic")
 #     finally:
 #         return module
 
+
 def retrieve_revision_script(dir: os.path, revision_id: str) -> tuple[str | None]:
     if os.path.exists(dir):
         files = os.listdir(dir)
         for script in files:
-            _, ext = os.path.splitext(script) 
-            full_script_path = os.path.join(dir, script);
+            _, ext = os.path.splitext(script)
+            full_script_path = os.path.join(dir, script)
             if ext in [".py", ".pyc", ".pyo"] and os.path.isfile(full_script_path):
                 # script_module = load_module_py(revision_id, full_script_path)
                 script_module = util.load_python_file(dir, script)
                 if script_module is None:
                     continue
-                if hasattr(script_module, 'revision') and hasattr(script_module, 'down_revision'):
+                if hasattr(script_module, "revision") and hasattr(
+                    script_module, "down_revision"
+                ):
                     if script_module.revision == revision_id:
-                        shutil.copy(full_script_path,os.path.join(ALEMBIC_DIR,"versions"))
+                        shutil.copy(
+                            full_script_path, os.path.join(ALEMBIC_DIR, "versions")
+                        )
                         return str(script), str(script_module.down_revision)
         return None, "ENOFOUND"
     return None, "ENODIR"
 
+
 def clear_retrieved_scripts(files: list[str]):
     versions_dir = os.path.join(ALEMBIC_DIR, "versions")
     logger.info("\n==== removing extra migration scripts ====\n\n")
-    
+
     if files.__len__() == 0:
         logger.info("no scripts retrieved")
         return
 
     for file in files:
-        file_abs_path = os.path.join(versions_dir,file)
+        file_abs_path = os.path.join(versions_dir, file)
         if os.path.isfile(file_abs_path):
             logger.info(f" * {file_abs_path}")
             os.remove(file_abs_path)
@@ -79,9 +81,7 @@ def update_db_migrations():
         logger.info("Starting database migration process")
 
         alembic_cfg = Config(ALEMBIC_CONFIG_FILE_PATH)
-        alembic_cfg.set_main_option(
-            "script_location", ALEMBIC_DIR
-        )
+        alembic_cfg.set_main_option("script_location", ALEMBIC_DIR)
         alembic_cfg.attributes["configure_logger"] = False
 
         script = ScriptDirectory.from_config(alembic_cfg)
@@ -91,12 +91,12 @@ def update_db_migrations():
             current_rev = MigrationContext.configure(connection).get_current_revision()
             logger.info(f"current DB revision: {current_rev}")
 
-        #the target revision is the latest migration path provided revision in the folder of version
+        # the target revision is the latest migration path provided revision in the folder of version
         target_rev = script.get_current_head()
 
         logger.info(f"Target revision set to: {target_rev}")
 
-        #update the migration scripts in the user space
+        # update the migration scripts in the user space
         versions_path = os.path.join(BASE_DIR, "alembic", "versions")
         migration_scripts = os.listdir(versions_path)
 
@@ -105,20 +105,22 @@ def update_db_migrations():
         for filename in migration_scripts:
             full_filename = os.path.join(versions_path, filename)
             if os.path.isfile(full_filename):
-                shutil.copy(full_filename,USER_DB_MIGRATION_DIR)
+                shutil.copy(full_filename, USER_DB_MIGRATION_DIR)
 
         if current_rev == target_rev:
             logger.info("Database is already at target version")
             return
-        #validate the current revision is in the revision list, if it is not, we are facing a downgrade, else an upgrade
+        # validate the current revision is in the revision list, if it is not, we are facing a downgrade, else an upgrade
         try:
-            #if it is we are going to update it
+            # if it is we are going to update it
             if current_rev is not None:
                 logger.info("looking for current revision in script dir")
-                script.get_revision(current_rev) # raises exception if does not found the revision
+                script.get_revision(
+                    current_rev
+                )  # raises exception if does not found the revision
                 # TODO: Look for the revision that revises the current rev
             logger.info(f"Starting upgrade from {current_rev} to {target_rev} (head)")
-            command.upgrade(alembic_cfg,"head")
+            command.upgrade(alembic_cfg, "head")
             logger.info("Database upgrade completed successfully")
 
         except util.CommandError as ce:
@@ -129,15 +131,17 @@ def update_db_migrations():
 
             # check the current version migration file is saved in user space. If so, retrieve them
             # until reach the "head" version
-            logger.info("revision not found in script dir, retrieving downgrade path from backup")
+            logger.info(
+                "revision not found in script dir, retrieving downgrade path from backup"
+            )
             # retrieved_file, next_id = retrieve_revision_script(USER_DB_MIGRATION_DIR,current_rev)
 
             next_id = current_rev
 
             # if retrieved_file is None:
             #     error = "backup dir not found" if next_id == "ENODIR" else "current revision migration script not found in backup dir"
-            #     raise Exception(error) 
-            
+            #     raise Exception(error)
+
             # logger.info(f"revision retrieved {current_rev}, points to {next_id} ")
             # retrieved_ids.append(current_rev)
             # retrieved_files.append(retrieved_file)
@@ -145,21 +149,31 @@ def update_db_migrations():
             while True:
                 if next_id in retrieved_ids:
                     raise Exception("circular path found while downgrading")
-                retrieved_file, aux_id = retrieve_revision_script(USER_DB_MIGRATION_DIR,next_id)
+                retrieved_file, aux_id = retrieve_revision_script(
+                    USER_DB_MIGRATION_DIR, next_id
+                )
                 if retrieved_file is None:
-                    error = "backup dir not found" if aux_id == "ENODIR" else "current revision migration script not found in backup dir"
-                    raise Exception(f"error retrieving revision {next_id} script: {aux_id}. Downgrade path broken: {error}")
-                
+                    error = (
+                        "backup dir not found"
+                        if aux_id == "ENODIR"
+                        else "current revision migration script not found in backup dir"
+                    )
+                    raise Exception(
+                        f"error retrieving revision {next_id} script: {aux_id}. Downgrade path broken: {error}"
+                    )
+
                 retrieved_files.append(retrieved_file)
                 retrieved_ids.append(next_id)
 
                 if aux_id is None:
-                    raise Exception(f"error retrieving downgrade path from {current_rev} to {target_rev}. Base reached")
-                
+                    raise Exception(
+                        f"error retrieving downgrade path from {current_rev} to {target_rev}. Base reached"
+                    )
+
                 next_id = aux_id
                 if next_id == target_rev:
                     break
-                
+
             # do the downgrade
             logger.info(f"Starting downgrade from {current_rev} to {target_rev}")
             command.downgrade(alembic_cfg, target_rev)
