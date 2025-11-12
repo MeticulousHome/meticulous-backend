f1b9c7c3091ae738d478c30469dd7dbef8a74c55
Angel-Padilla <angel.p.lopez@outlook.com>
2025-11-12 11:13:28 -0600

    chore: adequate logging of ESP Log messages

--- machine.py
+++ machine.py
@@ -425,49 +425,63 @@ class Machine:
                                 exc_info=True,
                             )
                     case ["Log", *log_data]:
-                        logger.info(data_str.strip("\r\n"))
-                        if len(log_data) > 1:
-                            log_level = log_data[0]
+
+                        def get_log_items(log_data: list[str]):
+                            for data_str in log_data[2:]:
+                                # data in the form: <key>=<value>
+                                data = data_str.split("=")
+                                if len(data) < 2:
+                                    logger.warning(
+                                        f"Error parsing ESP log item: {data_str}"
+                                    )
+                                    continue
+                                key = data[0]
+                                value = data[1]
+                                items.setdefault(key, value)
+
+                        # logger.info(data_str.strip("\r\n"))
+                        try:
+                            log_level = log_data[0].lower()
                             message = log_data[1]
+                            full_message = ",".join(log_data[1:])
                             items: dict[str, str] = {}
                             send_to_sentry = False
 
-                            def get_log_items(log_data: list[str]):
-                                for data_str in log_data[2:]:
-                                    # data in the form: <key>=<value>
-                                    data = data_str.split("=")
-                                    if len(data) < 2:
-                                        logger.warning(
-                                            f"Error parsing ESP log item: {data_str}"
-                                        )
-                                        continue
-                                    key = data[0]
-                                    value = data[1]
-                                    items.setdefault(key, value)
+                            match log_level:
 
-                            try:
-                                if len(log_data) > 2:
-                                    get_log_items(log_data=log_data)
-                                    send_to_sentry = (
-                                        items.get("sentry", "false") == "true"
-                                    )
-                                    new_dict = {
-                                        k: v for k, v in items.items() if k != "sentry"
-                                    }
-                                    if send_to_sentry:
-                                        with sentry_sdk.new_scope() as scope:
-                                            scope.set_context("esp-data", new_dict)
-                                            scope.set_client(ESPSentryClient)
-                                            scope.capture_message(
-                                                message=message,
-                                                level=log_level.lower(),
-                                            )
-                                # if we want to send to sentry
-                            except Exception as e:
-                                logger.error(
-                                    f"Error processing Log from ESP: {e}",
-                                    exc_info=True,
-                                )
+                                case "debug":
+                                    logger.debug(full_message)
+                                case "info":
+                                    logger.info(full_message)
+                                case "warning":
+                                    logger.warning(full_message)
+                            new_dict = None
+                            if len(log_data) > 2:
+                                get_log_items(log_data=log_data)
+                                send_to_sentry = items.get("sentry", "false") == "true"
+                                new_dict = {
+                                    k: v for k, v in items.items() if k != "sentry"
+                                }
+
+                            send_to_sentry = send_to_sentry or log_level == "error"
+
+                            if send_to_sentry:
+                                with sentry_sdk.new_scope() as scope:
+                                    if new_dict is not None:
+                                        scope.set_context("esp-data", new_dict)
+                                    scope.set_client(ESPSentryClient)
+                                    if log_level == "error":
+                                        logger.error(full_message)
+                                    else:
+                                        scope.capture_message(
+                                            message=message,
+                                            level=log_level,
+                                        )
+                        except Exception as e:
+                            logger.error(
+                                f"Error '{e}' processing Log from ESP: 'Log,{",".join(log_data)}'",
+                                exc_info=True,
+                            )
                     case [*_]:
                         logger.info(data_str.strip("\r\n"))
 
