7f8cfeb594a881a9f258b6461bb0c3868fd0adea
Angel-Padilla <angel.p.lopez@outlook.com>
2026-01-06 13:57:06 -0600

    feat: ponderate energy also using pressure generated chore: format code

--- monitoring/motor_power_monitoring.py
+++ monitoring/motor_power_monitoring.py
@@ -1,9 +1,9 @@
 from collections import deque
-from esp_serial.data import SensorData
+from esp_serial.data import SensorData, ShotData
 from log import MeticulousLogger
 import time
 
-INTEGRATION_WINDOW_TIME = 360 
+INTEGRATION_WINDOW_TIME = 360
 CONSTANT_DISSIPATED_ENERGY = 0
 logger = MeticulousLogger.getLogger(name=__name__)
 
@@ -11,8 +11,6 @@ logger = MeticulousLogger.getLogger(name=__name__)
 # numerically integrates the value provided in real time using the timestamp deltas as dT
 # along an integration window of INTEGRATION_WINDOW_TIME seconds
 
-import time
-from collections import deque
 
 class EnergyCalculator:
     def __init__(self, integration_window_seconds):
@@ -20,7 +18,7 @@ class EnergyCalculator:
         self.history: deque[tuple[float, float]] = deque()
         self.total_energy: float = 0.0
 
-    def calculate_motor_energy(self, sensors: SensorData):
+    def calculate_motor_energy(self, sensors: SensorData, shot: ShotData):
         """
         numerically integrates the value provided in real time using the timestamp deltas as dT
         along an integration window of INTEGRATION_WINDOW_TIME seconds
@@ -32,34 +30,38 @@ class EnergyCalculator:
         now = time.monotonic()
         power = (sensors.motor_power / 100.0) * sensors.motor_current
         if len(self.history) == 0:
-            self.history.append((0,now))
+            self.history.append((0, now))
             return float(0.0)
-        
+
         last_timestamp = self.history[-1][1]
         dT = now - last_timestamp
 
         if dT < 0:
             dT = 0.0
 
-        added_energy = power*dT 
+        added_energy = (
+            power * dT * shot.pressure if shot and shot.pressure else power * dT
+        )
         self.history.append((added_energy, now))
 
         self.total_energy += added_energy
-        self.total_energy = self.total_energy-CONSTANT_DISSIPATED_ENERGY if \
-                            self.total_energy-CONSTANT_DISSIPATED_ENERGY >= 0 else \
-                            self.total_energy
-         
+        self.total_energy = (
+            self.total_energy - CONSTANT_DISSIPATED_ENERGY
+            if self.total_energy - CONSTANT_DISSIPATED_ENERGY >= 0
+            else self.total_energy
+        )
+
         while self.history and (now - self.history[0][1] > self.window_seconds):
             self.total_energy -= self.history.popleft()[0]
 
-        ## remove oldest energy contribution
+        # remove oldest energy contribution
         return self.total_energy
-    
+
     def restart(self):
         self.history.clear()
         self.total_energy = 0.0
-        self.window_seconds = INTEGRATION_WINDOW_TIME 
-    
+        self.window_seconds = INTEGRATION_WINDOW_TIME
+
     def set_window_time(self, new_window_time: float | int):
         if not isinstance(new_window_time, (float, int)):
             raise TypeError(f"{new_window_time} is not a number")
@@ -69,5 +71,6 @@ class EnergyCalculator:
             now = self.history[-1][1]
             while self.history and now - self.history[0][1] > self.window_seconds:
                 self.total_energy -= self.history.popleft()[0]
-    
+
+
 motor_energy_calculator = EnergyCalculator(INTEGRATION_WINDOW_TIME)
