521fa01d500c8852ec2602bbcc9b96d5e0773a16
Angel-Padilla <angel.p.lopez@outlook.com>
2026-01-21 13:21:09 -0600

    feat: implement motor energy data saving thread to avoid losing it when turning the machine off

--- monitoring/motor_power_monitoring.py
+++ monitoring/motor_power_monitoring.py
@@ -2,10 +2,14 @@ from collections import deque
 from esp_serial.data import SensorData, ShotData
 from log import MeticulousLogger
 import time
+import os
+from threading import Thread
 
 INTEGRATION_WINDOW_TIME = 600
 CONSTANT_DISSIPATED_ENERGY = 0
 logger = MeticulousLogger.getLogger(name=__name__)
+
+MOTOR_ENERGY_PATH = "/meticulous-user/energy"
 
 # energy consumtion calculator
 # numerically integrates the value provided in real time using the timestamp deltas as dT
@@ -13,10 +17,26 @@ logger = MeticulousLogger.getLogger(name=__name__)
 
 
 class EnergyCalculator:
-    def __init__(self, integration_window_seconds):
+    def __init__(self, integration_window_seconds, name):
         self.window_seconds = integration_window_seconds
         self.history: deque[tuple[float, float]] = deque()
         self.total_energy: float = 0.0
+        self.save_thread: Thread = None
+        self.name: str = f"{name}_energy"
+        self.save_file_path = os.path.join(MOTOR_ENERGY_PATH, self.name)
+
+        # read the saved data from the nvs if it exist
+        if os.path.exists(self.save_file_path):
+            try:
+                with open(self.save_file_path, "r") as e_file:
+                    data = e_file.read()
+                    self.total_energy = float(data)
+            except Exception as e:
+                logger.warning(f"cannot read motor energy stored file: {e}")
+        logger.debug(f"starting {self.name}: {self.total_energy}")
+
+        # start saving data thread
+        self.start_data_save_thread()
 
     def calculate_motor_energy(self, sensors: SensorData, shot: ShotData):
         """
@@ -75,5 +95,30 @@ class EnergyCalculator:
             while self.history and now - self.history[0][1] > self.window_seconds:
                 self.total_energy -= self.history.popleft()[0]
 
+    def save_data_nvs(self):
+        if not os.path.exists(MOTOR_ENERGY_PATH):
+            os.makedirs(MOTOR_ENERGY_PATH, exist_ok=True)
+        with open(self.save_file_path, "w") as e_file:
+            e_file.write(str(self.total_energy))
 
-motor_energy_calculator = EnergyCalculator(INTEGRATION_WINDOW_TIME)
+    @staticmethod
+    def save_data_thread(instance):
+        if not isinstance(instance, EnergyCalculator):
+            return
+        logger.log("save_data_thread started")
+        # infinite loop, save data every second
+        while True:
+            instance.save_data_nvs()
+            time.sleep(1)
+
+    def start_data_save_thread(self):
+        if self.save_thread is None:
+            self.save_thread = Thread(
+                target=EnergyCalculator.save_data_thread, args=(self)
+            )
+            self.save_thread.start()
+        else:
+            logger.warning(f"{self.name} data saving thread has already started")
+
+
+motor_energy_calculator = EnergyCalculator(INTEGRATION_WINDOW_TIME, "motor")
