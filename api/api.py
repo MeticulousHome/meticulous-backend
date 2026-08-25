006256f2ca70c876f98f1aef0489a1e4da68c549
Angel-Padilla <angel.p.lopez@outlook.com>
2026-08-24 19:33:37 -0600

    Add idle screen bundle API

--- api/api.py
+++ api/api.py
@@ -54,6 +54,7 @@ class API:
         from . import serial as _serial  # noqa
         from . import password_handler as _password_handler  # noqa
         from . import smoke_validation as _smoke_validation  # noqa
+        from . import idle_screens as _idle_screens  # noqa
 
         routes = []
         logger.info("API Routes registered:")
